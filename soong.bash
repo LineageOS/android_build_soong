24679676cad44947ea8752d1d193b0541d942a16
Colin Cross <ccross@android.com>
2015-04-10 15:45:15 -0700

    Fix soong scripts to support directories with spaces
    Using a path with a space to execute soong is unlikely, but it
    might as well work.  Quote all the paths in the soong scripts.
    
    Soong and blueprint will still both fail if the relative path between
    the soong script and the source directory has a space in it, but this
    is even more unlikely.
    
    Change-Id: I8986f10115209d69b09b82ffea26e4b10d29c197

--- soong.bash
+++ soong.bash
@@ -3,24 +3,24 @@
 # Determine the build directory location based on the location of this script.
 BPBUILD="${BASH_SOURCE[0]}"
 BUILDDIR=`dirname "${BASH_SOURCE[0]}"`
-BOOTSTRAP=${BUILDDIR}/.soong.bootstrap
+BOOTSTRAP="${BUILDDIR}/.soong.bootstrap"
 
 # The source directory path and operating system will get written to
 # .soong.bootstrap by the bootstrap script.
 
-if [ ! -f ${BOOTSTRAP} ]; then
+if [ ! -f "${BOOTSTRAP}" ]; then
     echo "Error: soong script must be located in a directory created by bootstrap.bash"
     exit 1
 fi
 
-source ${BOOTSTRAP}
+source "${BOOTSTRAP}"
 
 if [[ ${SRCDIR_IN:0:1} == '/' ]]; then
     # SRCDIR_IN is an absolute path
-    SRCDIR=${SRCDIR_IN}
+    SRCDIR="${SRCDIR_IN}"
 else
     # SRCDIR_IN is a relative path
-    SRCDIR=${BUILDDIR}/${SRCDIR_IN}
+    SRCDIR="${BUILDDIR}/${SRCDIR_IN}"
 fi
 
 # Let Blueprint know that the Ninja we're using performs multiple passes that
@@ -30,18 +30,18 @@ export BLUEPRINT_NINJA_HAS_MULTIPASS=1
 # Ninja can't depend on environment variables, so do a manual comparison
 # of the relevant environment variables from the last build using the
 # soong_env tool and trigger a build manifest regeneration if necessary
-ENVFILE=${BUILDDIR}/.soong.environment
-ENVTOOL=${BUILDDIR}/.bootstrap/bin/soong_env
-if [ -f ${ENVFILE} ]; then
-    if [ -x ${ENVTOOL} ]; then
-        if ! ${ENVTOOL} ${ENVFILE}; then
+ENVFILE="${BUILDDIR}/.soong.environment"
+ENVTOOL="${BUILDDIR}/.bootstrap/bin/soong_env"
+if [ -f "${ENVFILE}" ]; then
+    if [ -x "${ENVTOOL}" ]; then
+        if ! "${ENVTOOL}" "${ENVFILE}"; then
             echo "forcing build manifest regeneration"
-            rm -f ${ENVFILE}
+            rm -f "${ENVFILE}"
         fi
     else
         echo "Missing soong_env tool, forcing build manifest regeneration"
-        rm -f ${ENVFILE}
+        rm -f "${ENVFILE}"
     fi
 fi
 
-${SRCDIR}/prebuilts/ninja/${PREBUILTOS}/ninja -C ${BUILDDIR} "$@"
+"${SRCDIR}/prebuilts/ninja/${PREBUILTOS}/ninja" -C "${BUILDDIR}" "$@"
