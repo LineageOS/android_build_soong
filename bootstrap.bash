98c93e9fbfa32735e40e2f6b28fce926d5c30e66
Dan Willemsen <dwillemsen@google.com>
2015-06-10 16:59:11 -0700

    Fix bootstrap.bash -r with Android.bp
    Change-Id: I8182e57105b1c17254e082f6b72d2504befb7870

--- bootstrap.bash
+++ bootstrap.bash
@@ -2,6 +2,7 @@
 
 export BOOTSTRAP="${BASH_SOURCE[0]}"
 export SRCDIR=$(dirname "${BASH_SOURCE[0]}")
+export TOPNAME="Android.bp"
 export BOOTSTRAP_MANIFEST="${SRCDIR}/build/soong/build.ninja.in"
 
 case $(uname) in
