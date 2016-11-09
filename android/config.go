74b8b55bb28629eb2a2d275f744688b34c2efa5e
Dan Albert <danalbert@google.com>
2016-11-09 19:39:42 +0000

    Revert "Use the platform SDK version from the config."
    This reverts commit 9bef52380933a3f1444607739d94a98506e0b441.
    
    Reason for revert: Broke things
    
    Change-Id: Ic3deac4fb6992572ac0cc95fa04d003e0c29f113

--- android/config.go
+++ android/config.go
@@ -323,12 +323,8 @@ func (c *config) PlatformVersion() string {
 	return "M"
 }
 
-func (c *config) PlatformSdkVersionInt() int {
-	return *c.ProductVariables.Platform_sdk_version
-}
-
 func (c *config) PlatformSdkVersion() string {
-	return strconv.Itoa(c.PlatformSdkVersionInt())
+	return strconv.Itoa(*c.ProductVariables.Platform_sdk_version)
 }
 
 func (c *config) BuildNumber() string {
