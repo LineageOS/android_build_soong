5f27ff23011beb3ed710452d9f2ad9ca28e50f9f
Abhisek Devkota <ciwrl@lineageos.org>
2017-02-08 12:06:13 -0800

    Add dumpvar for WITH_SU
     -Allow folks to see easily if the term session they are in has
       WITH_SU set to true
    
    Change-Id: Ib310d0016bb9eb16960f794720321b706b6fd8b0

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -200,6 +200,7 @@ var BannerVars = []string{
 	"BUILD_ID",
 	"OUT_DIR",
 	"SOONG_SDK_SNAPSHOT_TARGET_BUILD_RELEASE",
+	"WITH_SU",
 }
 
 func Banner(config Config, make_vars map[string]string) string {
