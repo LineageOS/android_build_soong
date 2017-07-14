b2e6c2e571058cfd9a3cf7a23927fb1c49d59523
Dan Willemsen <dwillemsen@google.com>
2017-07-13 17:24:44 -0700

    Switch product configs from make to ckati
    This speeds up dumping make variables from ~380ms using make to ~220ms
    using ckati. It also means that we're consistently using the same parser
    for builds (with the same .KATI_READONLY/etc extensions).
    
    envsetup.sh (lunch) / other scripts still use make, changing those to go
    through soong_ui will be a future change.
    
    Test: m clean; m nothing
    Test: USE_GOMA=true m nothing
    Test: m PRODUCT-aosp_x86-sdk
    Test: m APP-Calculator
    Test: build/soong/build_test.bash -only-config  (on AOSP and internal master)
    Change-Id: I6ca554de8de4955fb869001d06d29969b75751cc

--- ui/build/build.go
+++ ui/build/build.go
@@ -104,7 +104,7 @@ func checkCaseSensitivity(ctx Context, config Config) {
 
 func help(ctx Context, config Config, what int) {
 	cmd := Command(ctx, config, "help.sh", "build/make/help.sh")
-	cmd.Sandbox = makeSandbox
+	cmd.Sandbox = dumpvarsSandbox
 	cmd.Stdout = ctx.Stdout()
 	cmd.Stderr = ctx.Stderr()
 	cmd.RunOrFatal()
