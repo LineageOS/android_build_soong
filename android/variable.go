0c77f66b8b821b8f603a3c35f21ef960f53c5b78
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    soong: extend with lineage board flags
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"lineage/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -94,6 +96,9 @@ type variableProperties struct {
 		Pdk struct {
 			Enabled *bool
 		}
+
+		// include Lineage variables
+		*android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -167,6 +172,9 @@ type productVariables struct {
 	Override_rs_driver *string `json:",omitempty"`
 
 	DeviceKernelHeaders []string `json:",omitempty"`
+
+	// include Lineage variables
+	*android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
--- cmd/microfactory/microfactory.bash
+++ cmd/microfactory/microfactory.bash
@@ -84,6 +84,7 @@ function build_go
     rm -f "${out_dir}/.$1.trace"
     ${mf_cmd} -s "${mf_src}" -b "${mf_bin}" \
             -pkg-path "android/soong=${TOP}/build/soong" -trimpath "${TOP}/build/soong" \
+            -pkg-path "lineage/soong=${TOP}/vendor/lineage/build/soong" \
             -o "${built_bin}" $2
 
     if [ $from_src -eq 1 ]; then
