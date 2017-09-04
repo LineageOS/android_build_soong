661e0b426c7dcb1f1449714d5d651ff11c1d0a1c
Simon Shields <simon@lineageos.org>
2017-09-04 21:09:44 +1000

    soong: explicitly specify name of Lineage variables struct
    Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15

--- android/variable.go
+++ android/variable.go
@@ -98,7 +98,7 @@ type variableProperties struct {
 		}
 
 		// include Lineage variables
-		*android.Product_variables
+		Lineage android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -169,7 +169,7 @@ type productVariables struct {
 	Override_rs_driver *string `json:",omitempty"`
 
 	// include Lineage variables
-	*android.ProductVariables
+	Lineage android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
