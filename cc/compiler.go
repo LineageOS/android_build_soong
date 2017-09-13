4b040ab6a1816e614dfbda7859b904eab5f273f1
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-13 19:56:59 -0400

    soong: Include compilerFlagsLineage
    Change-Id: Id882372b4b20c41c2a06e55c64ec00f3ab9b0c31

--- cc/compiler.go
+++ cc/compiler.go
@@ -23,6 +23,8 @@ import (
 
 	"android/soong/android"
 	"android/soong/cc/config"
+
+	"lineage/soong/android"
 )
 
 // This file contains the basic C/C++/assembly to .o compliation steps
@@ -313,6 +315,8 @@ func (compiler *baseCompiler) compilerFlags(ctx ModuleContext, flags Flags) Flag
 		if Bool(ctx.AConfig().ProductVariables.Brillo) {
 			flags.GlobalFlags = append(flags.GlobalFlags, "-D__BRILLO__")
 		}
+
+		cc.compilerFlagsLineage
 
 		if ctx.Device() {
 			if Bool(compiler.Properties.Rtti) {
