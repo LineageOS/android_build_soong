0a32e5936b4c87bc705d06fa2d4f6fed1f58d602
Nan Zhang <nanzhang@google.com>
2017-12-01 06:19:27 +0000

    Revert "Change Python in Soong to support device side build."
    This reverts commit 3bba05962dc4329c6e3d28544775db591574ba7b.
    
    Reason for revert: <revert mac builds>
    
    Change-Id: I25f1380573d69f7e111287624a2d4a3d95fdd86b

--- python/library.go
+++ python/library.go
@@ -22,17 +22,10 @@ import (
 
 func init() {
 	android.RegisterModuleType("python_library_host", PythonLibraryHostFactory)
-	android.RegisterModuleType("python_library", PythonLibraryFactory)
 }
 
 func PythonLibraryHostFactory() android.Module {
 	module := newModule(android.HostSupportedNoCross, android.MultilibFirst)
-
-	return module.Init()
-}
-
-func PythonLibraryFactory() android.Module {
-	module := newModule(android.HostAndDeviceSupported, android.MultilibBoth)
 
 	return module.Init()
 }
