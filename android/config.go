c37c6343e24399d8238ebeb2ece665fcf660cc13
Alan Leung <acleung@google.com>
2017-12-13 00:28:49 -0800

    Prep D8 default instead of CompatDX in build/soong
    Bug: 69329508
    Test: m checkbuild tests
    Change-Id: Ibaa2d787c644cf25104af78305a6eed16028dcf0

--- android/config.go
+++ android/config.go
@@ -584,6 +584,10 @@ func (c *config) Android64() bool {
 	return false
 }
 
+func (c *config) UseD8Desugar() bool {
+	return c.IsEnvTrue("USE_D8_DESUGAR")
+}
+
 func (c *config) UseGoma() bool {
 	return Bool(c.ProductVariables.UseGoma)
 }
--- java/config/makevars.go
+++ java/config/makevars.go
@@ -46,12 +46,14 @@ func makeVarsProvider(ctx android.MakeVarsContext) {
 	ctx.Strict("JAVADOC", "${JavadocCmd}")
 	ctx.Strict("COMMON_JDK_FLAGS", "${CommonJdkFlags}")
 
-	if ctx.Config().IsEnvTrue("USE_D8_DESUGAR") {
+	if ctx.Config().UseD8Desugar() {
 		ctx.Strict("DX", "${D8Cmd}")
 		ctx.Strict("DX_COMMAND", "${D8Cmd} -JXms16M -JXmx2048M")
+		ctx.Strict("USE_D8_DESUGAR", "true")
 	} else {
 		ctx.Strict("DX", "${DxCmd}")
 		ctx.Strict("DX_COMMAND", "${DxCmd} -JXms16M -JXmx2048M")
+		ctx.Strict("USE_D8_DESUGAR", "false")
 	}
 
 	ctx.Strict("TURBINE", "${TurbineJar}")
