9840c0296ccc96d12d51a333aeac4f76479e0a61
AnilKumar Chimata <anilc@codeaurora.org>
2018-06-20 20:25:55 +0530

    android: variable: Add new flags for HW FDE
    Add new flags for HW FDE to pass CFlags to source code.
    
    CRs-Fixed: 2210986
    Change-Id: I70d68f015ee8eb8d123ce863ce8e36d0310bc082

--- android/variable.go
+++ android/variable.go
@@ -117,6 +117,16 @@ type variableProperties struct {
 			Srcs         []string
 		}
 
+		Device_support_hwfde struct {
+			Cflags []string
+			Header_libs  []string
+			Shared_libs  []string
+		}
+
+		Device_support_hwfde_perf struct {
+			Cflags []string
+		}
+
 		// include Lineage variables
 		Lineage android.Product_variables
 	} `android:"arch_variant"`
@@ -193,6 +203,8 @@ type productVariables struct {
 	Use_lmkd_stats_log         *bool `json:",omitempty"`
 	Arc                        *bool `json:",omitempty"`
 	MinimizeJavaDebugInfo      *bool `json:",omitempty"`
+	Device_support_hwfde       *bool `json:",omitempty"`
+	Device_support_hwfde_perf  *bool `json:",omitempty"`
 
 	IntegerOverflowExcludePaths *[]string `json:",omitempty"`
 
