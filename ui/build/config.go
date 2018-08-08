7f042fddf5c5a84a1ac23368339d6bc752e2651a
Aaron Kling <webgeek1234@gmail.com>
2018-08-07 19:21:36 -0500

    locale: add C.utf8
    Several Linux distros use utf8 instead of UTF-8
    
    Change-Id: I7e3dd73ec89bc34d7f2dc1834082cc90f9521ad1

--- ui/build/config.go
+++ ui/build/config.go
@@ -284,6 +284,9 @@ func (c *configImpl) configureLocale(ctx Context) {
 	// for others)
 	if inList("C.UTF-8", locales) {
 		c.environ.Set("LANG", "C.UTF-8")
+	} else if inList("C.utf8", locales) {
+		// These normalize to the same thing
+		c.environ.Set("LANG", "C.UTF-8")
 	} else if inList("en_US.UTF-8", locales) {
 		c.environ.Set("LANG", "en_US.UTF-8")
 	} else if inList("en_US.utf8", locales) {
