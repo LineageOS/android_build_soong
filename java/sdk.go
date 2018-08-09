db79c8a9d36af08d7bd633578fe250f3f6504bab
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-09 14:08:53 -0700

    soong: Squash of lineage-sdk bringup commits
    Squash of:
    
    Author: Rashed Abdel-Tawab <rashed@linux.com>
    Date:   Thu Aug 9 14:08:53 2018 -0700
        soong: Special case Lineage SDK
         * org.lineageos.platform-res.apk needs to be installed to /system/framework
         * org.lineageos.platform-res needs to be a dependency for
           org.lineageos.platform and org.lineageos.platform.internal
         * Add other special exceptions for org.lineageos.platform-res
        Change-Id: Ic617c07c086916005ea4b88f26d31c61691a45f8
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Thu Aug 30 15:33:16 2018 -0700
        soong: make org.lineage.platform-res depend on framework-res
        *) Allows us to build org.lineage.platform-res with no_framework_libs
           true (as is done for framework-res).
        *) Whilst we're here, undo a dependency loop prevention in aar.go
           that we added during sdk bringup to allow our platform res to build
           with no_framework_libs false.
        Change-Id: Ib452a2e45112baf5d61b70b4be1ce0c01dfd84e5
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Mon Feb 4 18:56:52 2019 +0100
        Always link org.lineageos.platform-res for org.lineageos.platform.sdk
        Test: m clean && m org.lineageos.platform.sdk
        Change-Id: I58956855bd4d1157e2582103c4861e7b384b4f73
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Fri Aug 31 10:52:29 2018 -0700
        soong: Allow framework to access lineage-sdk resources
        *) Make framework depend on lineage-sdk resource package
        *) Allows framework module to access lineage-sdk resources
           via usual org.lineageos.platform.internal.R paths.
        Change-Id: Ifd19d43d9308ac370ad40a499de16bf8ce204beb
    
    Change-Id: Icc18de5dfaa83fc0a1eda6f3704f3a92e1de0764

--- java/sdk.go
+++ java/sdk.go
@@ -116,13 +116,14 @@ func decodeSdkDep(ctx android.EarlyModuleContext, sdkContext android.SdkContext)
 		}
 	}
 
-	toModule := func(modules []string, res string, aidl android.Path) sdkDep {
+	toModule := func(modules []string, res string, lineageRes string, aidl android.Path) sdkDep {
 		return sdkDep{
 			useModule:          true,
 			bootclasspath:      append(modules, config.DefaultLambdaStubsLibrary),
 			systemModules:      "core-current-stubs-system-modules",
 			java9Classpath:     modules,
 			frameworkResModule: res,
+			lineageResModule:   lineageRes,
 			aidl:               android.OptionalPathForPath(aidl),
 		}
 	}
@@ -135,6 +136,7 @@ func decodeSdkDep(ctx android.EarlyModuleContext, sdkContext android.SdkContext)
 			bootclasspath:      corePlatformBootclasspathLibraries(ctx),
 			classpath:          config.FrameworkLibraries,
 			frameworkResModule: "framework-res",
+			lineageResModule:   "org.lineageos.platform-res",
 		}
 	case android.SdkNone:
 		systemModules := sdkContext.SystemModules()
@@ -161,11 +163,11 @@ func decodeSdkDep(ctx android.EarlyModuleContext, sdkContext android.SdkContext)
 			noFrameworksLibs: true,
 		}
 	case android.SdkPublic:
-		return toModule([]string{"android_stubs_current"}, "framework-res", sdkFrameworkAidlPath(ctx))
+		return toModule([]string{"android_stubs_current"}, "framework-res", "org.lineageos.platform-res", sdkFrameworkAidlPath(ctx))
 	case android.SdkSystem:
-		return toModule([]string{"android_system_stubs_current"}, "framework-res", sdkFrameworkAidlPath(ctx))
+		return toModule([]string{"android_system_stubs_current"}, "framework-res", "org.lineageos.platform-res", sdkFrameworkAidlPath(ctx))
 	case android.SdkTest:
-		return toModule([]string{"android_test_stubs_current"}, "framework-res", sdkFrameworkAidlPath(ctx))
+		return toModule([]string{"android_test_stubs_current"}, "framework-res", "org.lineageos.platform-res", sdkFrameworkAidlPath(ctx))
 	case android.SdkCore:
 		return sdkDep{
 			useModule:        true,
@@ -181,6 +183,7 @@ func decodeSdkDep(ctx android.EarlyModuleContext, sdkContext android.SdkContext)
 			systemModules:      "core-module-lib-stubs-system-modules",
 			java9Classpath:     []string{"android_module_lib_stubs_current"},
 			frameworkResModule: "framework-res",
+			lineageResModule:   "org.lineageos.platform-res",
 			aidl:               android.OptionalPathForPath(nonUpdatableFrameworkAidlPath(ctx)),
 		}
 	case android.SdkSystemServer:
@@ -191,6 +194,7 @@ func decodeSdkDep(ctx android.EarlyModuleContext, sdkContext android.SdkContext)
 			systemModules:      "core-module-lib-stubs-system-modules",
 			java9Classpath:     []string{"android_system_server_stubs_current"},
 			frameworkResModule: "framework-res",
+			lineageResModule:   "org.lineageos.platform-res",
 			aidl:               android.OptionalPathForPath(sdkFrameworkAidlPath(ctx)),
 		}
 	default:
