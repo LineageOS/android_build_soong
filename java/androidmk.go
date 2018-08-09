2ddea2921a54c2e1ef2e682b7e58a573621511c8
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-09 14:08:53 -0700

    soong: Squash of lineage-sdk bringup commits
    Squash of:
    
    Author: Rashed Abdel-Tawab <rashed@linux.com>
    Date:   Thu Aug 9 14:08:53 2018 -0700
        soong: Special case Lineage SDK
         * org.lineageos.platform-res.apk needs to be installed to /system/framework
         * org.lineageos.platform-res needs to be a dependency for
           org.lineageos.platform and org.lineageos.platform.internal
         * Add other special exceptions for org.lineageos.platform-res
        Change-Id: Ic617c07c086916005ea4b88f26d31c61691a45f8
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Thu Aug 30 15:33:16 2018 -0700
        soong: make org.lineage.platform-res depend on framework-res
        *) Allows us to build org.lineage.platform-res with no_framework_libs
           true (as is done for framework-res).
        *) Whilst we're here, undo a dependency loop prevention in aar.go
           that we added during sdk bringup to allow our platform res to build
           with no_framework_libs false.
        Change-Id: Ib452a2e45112baf5d61b70b4be1ce0c01dfd84e5
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Mon Feb 4 18:56:52 2019 +0100
        Always link org.lineageos.platform-res for org.lineageos.platform.sdk
        Test: m clean && m org.lineageos.platform.sdk
        Change-Id: I58956855bd4d1157e2582103c4861e7b384b4f73
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Fri Aug 31 10:52:29 2018 -0700
        soong: Allow framework to access lineage-sdk resources
        *) Make framework depend on lineage-sdk resource package
        *) Allows framework module to access lineage-sdk resources
           via usual org.lineageos.platform.internal.R paths.
        Change-Id: Ifd19d43d9308ac370ad40a499de16bf8ce204beb
    
    Change-Id: Icc18de5dfaa83fc0a1eda6f3704f3a92e1de0764

--- java/androidmk.go
+++ java/androidmk.go
@@ -338,7 +338,7 @@ func (app *AndroidApp) AndroidMkEntries() []android.AndroidMkEntries {
 					entries.SetPath("LOCAL_SOONG_PROGUARD_DICT", app.proguardDictionary)
 				}
 
-				if app.Name() == "framework-res" {
+				if app.Name() == "framework-res" || app.Name() == "org.lineageos.platform-res" {
 					entries.SetString("LOCAL_MODULE_PATH", "$(TARGET_OUT_JAVA_LIBRARIES)")
 					// Make base_rules.mk not put framework-res in a subdirectory called
 					// framework_res.
@@ -467,7 +467,7 @@ func (a *AndroidLibrary) AndroidMkEntries() []android.AndroidMkEntries {
 			entries.SetPath("LOCAL_SOONG_AAR", a.aarFile)
 		}
 
-		if a.Name() == "framework-res" {
+		if a.Name() == "framework-res" || a.Name() == "org.lineageos.platform-res" {
 			entries.SetString("LOCAL_MODULE_PATH", "$(TARGET_OUT_JAVA_LIBRARIES)")
 			// Make base_rules.mk not put framework-res in a subdirectory called
 			// framework_res.
