b2afb1bac538a731228e86f522be103dddbbf8b1
Sam Mortimer <sam@mortimer.me.uk>
2018-08-17 11:25:08 -0700

    soong: Add java sources overlay support
    Change-Id: I94143febb0a8afa6a165364d36a40d5120a4e7bc

--- android/config.go
+++ android/config.go
@@ -27,6 +27,8 @@ import (
 
 	"github.com/google/blueprint/bootstrap"
 	"github.com/google/blueprint/proptools"
+
+	"lineage/soong/android"
 )
 
 var Bool = proptools.Bool
@@ -484,6 +486,14 @@ func (c *config) DeviceResourceOverlays() []string {
 
 func (c *config) ProductResourceOverlays() []string {
 	return c.productVariables.ProductResourceOverlays
+}
+
+func (c *config) JavaSourceOverlays() string {
+	return String(c.productVariables.Lineage.Java_Source_Overlays)
+}
+
+func (c *config) JavaSourceOverlayModuleWhitelist() []string {
+	return android.LineageConfig.JavaSourceOverlayModuleWhitelist
 }
 
 func (c *config) PlatformVersionName() string {
