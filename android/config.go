65a4235247890cbd229ca8291f697b315405c57e
Bruno Martins <bgcngm@gmail.com>
2018-09-03 01:34:18 +0100

    soong: Add function to return targer specific header path
    Change-Id: I8cc41d0ad93822866e8ea66507e4acf379fd3119

--- android/config.go
+++ android/config.go
@@ -780,6 +780,10 @@ func (c *deviceConfig) SpecificCameraParametersLibrary() string {
 	return String(c.config.productVariables.Lineage.Specific_camera_parameter_library)
 }
 
+func (c *deviceConfig) TargetSpecificHeadersIncludeDir() string {
+	return String(c.config.productVariables.Lineage.TargetSpecificHeadersIncludeDir)
+}
+
 func (c *deviceConfig) NativeCoverageEnabled() bool {
 	return Bool(c.config.productVariables.NativeCoverage)
 }
