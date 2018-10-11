ad73b2e18a8baddef74f69ded8efa4fc502a626d
Kostya Kortchinsky <kostyak@google.com>
2018-10-11 08:38:39 -0700

    Scudo minimal runtime support for Soong
    Scudo is now compatible with the -fsanitize-minimal-runtime, and offers a new
    dynamic library that doesn't bundle UBSan.
    
    This patch adds support for this new library in Soong, preferring it over the
    full one, unless a UBSan or diagnostic dependency is found.
    
    Test: aosp compiled with m -j
    Test: local test enabling Scudo for tombstoned
    Change-Id: I17794131db148b33f8a8710ac43302cadf1af314

--- cc/makevars.go
+++ cc/makevars.go
@@ -281,6 +281,7 @@ func makeVarsToolchain(ctx android.MakeVarsContext, secondPrefix string,
 		ctx.Strict(secondPrefix+"UBSAN_MINIMAL_RUNTIME_LIBRARY", strings.TrimSuffix(config.UndefinedBehaviorSanitizerMinimalRuntimeLibrary(toolchain), ".a"))
 		ctx.Strict(secondPrefix+"TSAN_RUNTIME_LIBRARY", strings.TrimSuffix(config.ThreadSanitizerRuntimeLibrary(toolchain), ".so"))
 		ctx.Strict(secondPrefix+"SCUDO_RUNTIME_LIBRARY", strings.TrimSuffix(config.ScudoRuntimeLibrary(toolchain), ".so"))
+		ctx.Strict(secondPrefix+"SCUDO_MINIMAL_RUNTIME_LIBRARY", strings.TrimSuffix(config.ScudoMinimalRuntimeLibrary(toolchain), ".so"))
 	}
 
 	// This is used by external/gentoo/...
