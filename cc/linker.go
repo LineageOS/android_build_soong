6d8c0a50bc3903f90935e5163e1b9bb46a30d5a9
Pirama Arumuga Nainar <pirama@google.com>
2019-04-15 23:03:38 -0700

    Revert "Revert "Enable lld for windows""
    This reverts commit 61166dc047ff30869667a58cf0daca68d20e38ab.
    
    One difference from the earlier change is that import libraries are now
    using the '.lib' extension instead of '.a' to prevent clash with
    AdbWinApi.a.
    
    Bug: http://b/110800681
    
    The following flags that the binutils linkers support are not
    available in lld for Windows:
      -soname
      --no-undefined
      -rpath
    
    Windows also uses "import libraries", which are stub libraries used only
    for linking.  The binutils linkers accepted a DLL and treated them as an
    import library.  But lld issues the following error:
    
      lld-link: error: ...DLL: bad file type. Did you specify a DLL instead
      of an import library?
    
    To resolve this, pass '-out-implib=libFoo.lib' to lld when linking
    libFoo.dll to get lld to generate an import library.  Add libFoo.lib as
    an implicit output to the 'ld' build rule.
    
    Rewrite the shared libraries when building a library/binary to use the
    import library instead of the DLL.  As a side-effect, this also uses the
    newly-created AdbWinApi.lib that's alongside
    development/host/windows/prebuilt/usb/AdbWinApi.dll
    
    Test: Run Windows tests (go/android-llvm-windows-testing) and check
    absence of regressions.  Also check that the following commands pass:
          $ adb.exe devices
          $ fastboot.exe devices
    
    Change-Id: I34e07d345e0207086ac8e8ea12525d8c322b20fd

--- cc/linker.go
+++ cc/linker.go
@@ -301,10 +301,6 @@ func (linker *baseLinker) useClangLld(ctx ModuleContext) bool {
 	if ctx.Darwin() {
 		return false
 	}
-	// http://b/110800681 - lld cannot link Android's Windows modules yet.
-	if ctx.Windows() {
-		return false
-	}
 	if linker.Properties.Use_clang_lld != nil {
 		return Bool(linker.Properties.Use_clang_lld)
 	}
@@ -358,7 +354,7 @@ func (linker *baseLinker) linkerFlags(ctx ModuleContext, flags Flags) Flags {
 			// darwin defaults to treating undefined symbols as errors
 			flags.LdFlags = append(flags.LdFlags, "-Wl,-undefined,dynamic_lookup")
 		}
-	} else if !ctx.Darwin() {
+	} else if !ctx.Darwin() && !ctx.Windows() {
 		flags.LdFlags = append(flags.LdFlags, "-Wl,--no-undefined")
 	}
 
@@ -395,7 +391,7 @@ func (linker *baseLinker) linkerFlags(ctx ModuleContext, flags Flags) Flags {
 
 	flags.LdFlags = append(flags.LdFlags, proptools.NinjaAndShellEscapeList(linker.Properties.Ldflags)...)
 
-	if ctx.Host() {
+	if ctx.Host() && !ctx.Windows() {
 		rpath_prefix := `\$$ORIGIN/`
 		if ctx.Darwin() {
 			rpath_prefix = "@loader_path/"
