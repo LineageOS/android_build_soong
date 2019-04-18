5c87791a78fc31620b165300c7976d8cf5674f9c
Colin Cross <ccross@android.com>
2019-04-18 14:27:12 -0700

    Allow codename.fingerprint format for targetSdkVersion
    Use codename.fingerprint format for targetSdkVersion if it is unset
    in the manifest and UNBUNDLED_BUILD_TARGET_SDK_WITH_API_FINGERPRINT=true.
    
    Test: manual
    Bug: 130541924
    Change-Id: I4e3b1274cc32038b00b292dc6d67559eb320e9e4
    Merged-In: I4e3b1274cc32038b00b292dc6d67559eb320e9e4
    (cherry picked from commit 109328714ff626446e16cfdd3103a5b969284841)

--- android/config.go
+++ android/config.go
@@ -618,7 +618,7 @@ func (c *config) UnbundledBuild() bool {
 	return Bool(c.productVariables.Unbundled_build)
 }
 
-func (c *config) UnbundledBuildPrebuiltSdks() bool {
+func (c *config) UnbundledBuildUsePrebuiltSdks() bool {
 	return Bool(c.productVariables.Unbundled_build) && !Bool(c.productVariables.Unbundled_build_sdks_from_source)
 }
 
