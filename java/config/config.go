4af387c20e4d72bf2997a405e344f818f2891da2
Colin Cross <ccross@android.com>
2019-05-16 13:16:29 -0700

    Move manifest_fixer to a python_binary_host module
    In preparation for making it use multiple files so that some
    implementation can be shared with a new tool.
    
    Also make manifest_fixer_test a python_test_host, and add it
    to TEST_MAPPING.
    
    Test: m checkbuild
    Change-Id: Iaae177efcb978187b126d90359b82cdfd3176857

--- java/config/config.go
+++ java/config/config.go
@@ -142,7 +142,7 @@ func init() {
 
 	hostBinToolVariableWithPrebuilt("Aapt2Cmd", "prebuilts/sdk/tools", "aapt2")
 
-	pctx.SourcePathVariable("ManifestFixerCmd", "build/soong/scripts/manifest_fixer.py")
+	pctx.HostBinToolVariable("ManifestFixerCmd", "manifest_fixer")
 
 	pctx.HostBinToolVariable("ManifestMergerCmd", "manifest-merger")
 
--- scripts/manifest_fixer_test.py
+++ scripts/manifest_fixer_test.py
@@ -425,4 +425,4 @@ class AddExtractNativeLibsTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-  unittest.main()
+  unittest.main(verbosity=2)
