7b8a567f44b3fdb30eac8211e8ed8e27fb162797
Colin Cross <ccross@android.com>
2019-05-28 14:06:17 -0700

    Don't build hiddenapi flags or encode dex for unbundled builds
    Builds with TARGET_BUILD_APPS shouldn't build the hiddenapi flags
    or encode dex files even if frameworks/base exists.
    
    Bug: 133343287
    Test: mainline modules build
    Change-Id: I0647451420fd09bb680808b35e1ad3b8f514ba46
    Merged-In: I0647451420fd09bb680808b35e1ad3b8f514ba46
    (cherry picked from commit 9c74a1ee855b1dcf4871a4cc40f317bd765ab7fd)

--- java/java.go
+++ java/java.go
@@ -1270,9 +1270,11 @@ func (j *Module) compile(ctx android.ModuleContext, extraSrcJars ...android.Path
 			return
 		}
 
-		// Hidden API CSV generation and dex encoding
-		dexOutputFile = j.hiddenAPI.hiddenAPI(ctx, dexOutputFile, j.implementationJarFile,
-			j.deviceProperties.UncompressDex)
+		if !ctx.Config().UnbundledBuild() {
+			// Hidden API CSV generation and dex encoding
+			dexOutputFile = j.hiddenAPI.hiddenAPI(ctx, dexOutputFile, j.implementationJarFile,
+				j.deviceProperties.UncompressDex)
+		}
 
 		// merge dex jar with resources if necessary
 		if j.resourceJar != nil {
