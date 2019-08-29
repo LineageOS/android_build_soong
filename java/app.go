66f60ec1145c1ec98db9b7ace4d353656df944cf
Chen Xu <fionaxu@google.com>
2019-08-29 16:23:52 -0700

    remove telephony-common from boot class path
    Bug: Bug: 135955937
    Test: Build & Manual
    Change-Id: I45c2b68b9045ed1b480a8df13a1a4adf9574d43e

--- java/app.go
+++ java/app.go
@@ -1106,7 +1106,8 @@ func (u *usesLibrary) deps(ctx android.BottomUpMutatorContext, hasFrameworkLibs 
 			ctx.AddVariationDependencies(nil, usesLibTag,
 				"org.apache.http.legacy",
 				"android.hidl.base-V1.0-java",
-				"android.hidl.manager-V1.0-java")
+				"android.hidl.manager-V1.0-java",
+				"telephony-common",)
 		}
 	}
 }
