90d29bf4ba583fd995b0f9fefa3342200eb00170
Sam Mortimer <sam@mortimer.me.uk>
2019-10-07 11:41:14 -0700

    soong: Give priority to modules in exported namespaces for bootjars
    * Looking for modules that provide a boot jar is currently
      done by module name match only.  This breaks when you
      have multiple copies of the same module in your build
      tree (eg in another device).
    
    * Add a new mutator that marks the modules that should be used
      for bootjar content.
    
    * The logic is simple: give priority to modules that are in
      exported soong namespace.
    
    Change-Id: I2476174b892475c14a9f10b5e66b8496186f81c0

--- apex/apex_test.go
+++ apex/apex_test.go
@@ -199,6 +199,7 @@ func testApexContext(_ *testing.T, bp string, handlers ...testCustomizer) (*andr
 
 	// from android package
 	android.RegisterPackageBuildComponents(ctx)
+	ctx.PreArchMutators(android.RegisterBootJarMutators)
 	ctx.PreArchMutators(android.RegisterVisibilityRuleChecker)
 
 	ctx.RegisterModuleType("apex", BundleFactory)
@@ -4704,6 +4705,7 @@ func testNoUpdatableJarsInBootImage(t *testing.T, errmsg, bp string, transformDe
 	ctx.RegisterModuleType("apex", BundleFactory)
 	ctx.RegisterModuleType("apex_key", ApexKeyFactory)
 	ctx.RegisterModuleType("filegroup", android.FileGroupFactory)
+	ctx.PreArchMutators(android.RegisterBootJarMutators)
 	ctx.PreArchMutators(android.RegisterDefaultsPreArchMutators)
 	cc.RegisterRequiredBuildComponentsForTest(ctx)
 	java.RegisterJavaBuildComponents(ctx)
@@ -4914,6 +4916,7 @@ func testApexPermittedPackagesRules(t *testing.T, errmsg, bp string, apexBootJar
 	ctx := android.NewTestArchContext()
 	ctx.RegisterModuleType("apex", BundleFactory)
 	ctx.RegisterModuleType("apex_key", ApexKeyFactory)
+	ctx.PreArchMutators(android.RegisterBootJarMutators)
 	ctx.PreArchMutators(android.RegisterDefaultsPreArchMutators)
 	cc.RegisterRequiredBuildComponentsForTest(ctx)
 	java.RegisterJavaBuildComponents(ctx)
