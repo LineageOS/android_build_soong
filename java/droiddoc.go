3b8f1cda4597783185cceb602852e5d22419ff40
Adrian Roos <roosa@google.com>
2019-11-01 13:42:39 +0100

    API Lint: Archive lint errors
    Adds reported lint errors to dist. This lets us track them and
    build tooling on top.
    
    Bug: 138440867
    Test: make dist droid; ls -l out/dist/apilint
    Change-Id: I75629e3d9e1a53d161d844dfac121a5998429e04

--- java/droiddoc.go
+++ java/droiddoc.go
@@ -1183,6 +1183,7 @@ type Droidstubs struct {
 	updateCurrentApiTimestamp     android.WritablePath
 	checkLastReleasedApiTimestamp android.WritablePath
 	apiLintTimestamp              android.WritablePath
+	apiLintReport                 android.WritablePath
 
 	checkNullabilityWarningsTimestamp android.WritablePath
 
@@ -1535,6 +1536,8 @@ func (d *Droidstubs) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 		} else {
 			cmd.Flag("--api-lint")
 		}
+		d.apiLintReport = android.PathForModuleOut(ctx, "api_lint_report.txt")
+		cmd.FlagWithOutput("--report-even-if-suppressed ", d.apiLintReport)
 
 		d.inclusionAnnotationsFlags(ctx, cmd)
 		d.mergeAnnoDirFlags(ctx, cmd)
