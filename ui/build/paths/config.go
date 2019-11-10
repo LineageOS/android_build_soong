487820df99fb1c74fbc67892a7493a4ed1e43269
Arne Coucheron <arco68@gmail.com>
2019-11-10 01:24:01 +0100

    Add lzma to allowed host tools
    Needed for "core: allow specifying building ramdisk(s) with lzma"
    in build/make.
    
    Change-Id: Ibff74efed1cecb4775187070958db872ab1e2d0a

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -93,6 +93,7 @@ var Configuration = map[string]PathConfig{
 	"java":     Allowed,
 	"javap":    Allowed,
 	"lsof":     Allowed,
+	"lzma":     Allowed,
 	"m4":       Allowed,
 	"nproc":    Allowed,
 	"openssl":  Allowed,
