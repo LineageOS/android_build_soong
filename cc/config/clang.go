45709ee8f1a659e1263535a62734c2f6ad1ba59f
Chih-Hung Hsieh <chh@google.com>
2020-02-14 13:25:09 -0800

    Enable tautological-bitwise-compare warning
    Bug: 148831363
    Test: make
    Change-Id: I9d1ba8ef50630e40d59e402c37ad1366a8e25ea6

--- cc/config/clang.go
+++ cc/config/clang.go
@@ -174,7 +174,6 @@ func init() {
 		"-Wno-bool-operation",                  // http://b/148287141
 		"-Wno-int-in-bool-context",             // http://b/148287349
 		"-Wno-sizeof-array-div",                // http://b/148815709
-		"-Wno-tautological-bitwise-compare",    // http://b/148831363
 		"-Wno-tautological-overlap-compare",    // http://b/148815696
 	}, " "))
 
