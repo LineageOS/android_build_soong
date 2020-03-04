690c8b8b5e62c47b545bc7c37262287bb02e7189
Yabin Cui <yabinc@google.com>
2020-03-04 15:39:51 -0800

    Switch from clang-r377782b to clang-r377782c.
    Bug: 149849077
    Test: N/A
    Change-Id: Id8b5dd13e87d1acd8dab1431fa6859721553c49c
    (cherry picked from commit 3125c990edc113b8dcc694fd436c2fffaf1abcdd)

--- cc/config/global.go
+++ cc/config/global.go
@@ -127,8 +127,8 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r377782b"
-	ClangDefaultShortVersion = "10.0.4"
+	ClangDefaultVersion      = "clang-r377782c"
+	ClangDefaultShortVersion = "10.0.5"
 
 	// Directories with warnings from Android.bp files.
 	WarningAllowedProjects = []string{
