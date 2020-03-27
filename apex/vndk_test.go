73d20d086ddef4e3116e8ae41efc0e6edc9bcc7e
Jooyung Han <jooyung@google.com>
2020-03-27 16:06:55 +0900

    Build VNDK APEX for VNDK-Lite
    For VNDK-Lite devices, which doesn't define BOARD_VNDK_VERSION, VNDK
    APEX is built with only VNDK-Sp libraries with core variants.
    
    Bug: 141908078
    Bug: 152353068
    Bug: 151635128
    Test: TH
    Merged-In: I0d08d32473368fd158818d4c2c72fc4cfad68ce6
    Change-Id: I0d08d32473368fd158818d4c2c72fc4cfad68ce6
    (cherry picked from commit 65d8a6262c6dd5ec4be8f751df457adbd246c471)
    
    Exempt-From-Owner-Approval: cp from internal
    Change-Id: I0d08d32473368fd158818d4c2c72fc4cfad68ce6

--- apex/vndk_test.go
+++ apex/vndk_test.go
@@ -8,6 +8,59 @@ import (
 	"android/soong/android"
 )
 
+func TestVndkApexForVndkLite(t *testing.T) {
+	ctx, _ := testApex(t, `
+		apex_vndk {
+			name: "myapex",
+			key: "myapex.key",
+		}
+
+		apex_key {
+			name: "myapex.key",
+			public_key: "testkey.avbpubkey",
+			private_key: "testkey.pem",
+		}
+
+		cc_library {
+			name: "libvndk",
+			srcs: ["mylib.cpp"],
+			vendor_available: true,
+			vndk: {
+				enabled: true,
+			},
+			system_shared_libs: [],
+			stl: "none",
+			apex_available: [ "myapex" ],
+		}
+
+		cc_library {
+			name: "libvndksp",
+			srcs: ["mylib.cpp"],
+			vendor_available: true,
+			vndk: {
+				enabled: true,
+				support_system_process: true,
+			},
+			system_shared_libs: [],
+			stl: "none",
+			apex_available: [ "myapex" ],
+		}
+	`+vndkLibrariesTxtFiles("current"), func(fs map[string][]byte, config android.Config) {
+		config.TestProductVariables.DeviceVndkVersion = proptools.StringPtr("")
+	})
+	// VNDK-Lite contains only core variants of VNDK-Sp libraries
+	ensureExactContents(t, ctx, "myapex", "android_common_image", []string{
+		"lib/libvndksp.so",
+		"lib/libc++.so",
+		"lib64/libvndksp.so",
+		"lib64/libc++.so",
+		"etc/llndk.libraries.VER.txt",
+		"etc/vndkcore.libraries.VER.txt",
+		"etc/vndksp.libraries.VER.txt",
+		"etc/vndkprivate.libraries.VER.txt",
+	})
+}
+
 func TestVndkApexUsesVendorVariant(t *testing.T) {
 	bp := `
 		apex_vndk {
