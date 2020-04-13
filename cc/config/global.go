c726ab533c4888163926620928dbd4ad700f15e4
Ramy Medhat <abdelaal@google.com>
2020-04-13 13:21:23 -0400

    [DO NOT MERGE] Allow remote execution of link actions.
    This CL adds a remoteexec package that allows adding a configurable RBE
    prefix to the template.
    
    Bug: b/166182389
    Test: built aosp crosshatch userdebug with and without RBE_CXX_LINKS.
    Change-Id: Ica920c3d7f79f2996210b9cbd448126451c1707c
    Merged-In: Ica920c3d7f79f2996210b9cbd448126451c1707c

--- cc/config/global.go
+++ cc/config/global.go
@@ -18,6 +18,7 @@ import (
 	"strings"
 
 	"android/soong/android"
+	"android/soong/remoteexec"
 )
 
 var (
@@ -232,6 +233,9 @@ func init() {
 		}
 		return ""
 	})
+
+	pctx.VariableFunc("RECXXLinksPool", envOverrideFunc("RBE_CXX_LINKS_POOL", remoteexec.DefaultPool))
+	pctx.VariableFunc("RECXXLinksExecStrategy", envOverrideFunc("RBE_CXX_LINKS_EXEC_STRATEGY", remoteexec.LocalExecStrategy))
 }
 
 var HostPrebuiltTag = pctx.VariableConfigMethod("HostPrebuiltTag", android.Config.PrebuiltOS)
@@ -244,4 +248,13 @@ func bionicHeaders(kernelArch string) string {
 		"-isystem bionic/libc/kernel/android/scsi",
 		"-isystem bionic/libc/kernel/android/uapi",
 	}, " ")
+}
+
+func envOverrideFunc(envVar, defaultVal string) func(ctx android.PackageVarContext) string {
+	return func(ctx android.PackageVarContext) string {
+		if override := ctx.Config().Getenv(envVar); override != "" {
+			return override
+		}
+		return defaultVal
+	}
 }
