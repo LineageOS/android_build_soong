c1be5ca62959c792ee5f8c6da2b843cafbce7135
Ramy Medhat <abdelaal@google.com>
2020-05-07 06:56:47 -0400

    Add support for the remote execution of Abi links.
    Test: built crosshatch userdebug with RBE_ABI_LINKS=1
    Bug: b/166742236
    Change-Id: Idb4e15b59878af4a98d9a594127c21deff669e16
    Merged-In: Idb4e15b59878af4a98d9a594127c21deff669e16

--- cc/config/global.go
+++ cc/config/global.go
@@ -261,6 +261,7 @@ func init() {
 	pctx.VariableFunc("RECXXLinksPool", remoteexec.EnvOverrideFunc("RBE_CXX_LINKS_POOL", remoteexec.DefaultPool))
 	pctx.VariableFunc("RECXXLinksExecStrategy", remoteexec.EnvOverrideFunc("RBE_CXX_LINKS_EXEC_STRATEGY", remoteexec.LocalExecStrategy))
 	pctx.VariableFunc("REAbiDumperExecStrategy", remoteexec.EnvOverrideFunc("RBE_ABI_DUMPER_EXEC_STRATEGY", remoteexec.LocalExecStrategy))
+	pctx.VariableFunc("REAbiLinkerExecStrategy", remoteexec.EnvOverrideFunc("RBE_ABI_LINKER_EXEC_STRATEGY", remoteexec.LocalExecStrategy))
 }
 
 var HostPrebuiltTag = pctx.VariableConfigMethod("HostPrebuiltTag", android.Config.PrebuiltOS)
