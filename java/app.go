966b2f02fb0587c32a208e3140443705c5c1d2b2
Liz Kammer <eakammer@google.com>
2020-05-19 16:15:25 -0700

    Add signing lineage for runtime_resource_overlay
    Add ability to specify a signing lineage file for
    runtime_resource_overlay.
    
    Test: app_test.go
    Bug: 153366049
    Merged-In: I68286bc0caa9daad6ef74db39f7750924f1d93dd
    Change-Id: I68286bc0caa9daad6ef74db39f7750924f1d93dd

--- java/app.go
+++ java/app.go
@@ -1692,6 +1692,9 @@ type RuntimeResourceOverlayProperties struct {
 	// module name in the form ":module".
 	Certificate *string
 
+	// Name of the signing certificate lineage file.
+	Lineage *string
+
 	// optional theme name. If specified, the overlay package will be applied
 	// only when the ro.boot.vendor.overlay.theme system property is set to the same value.
 	Theme *string
@@ -1766,7 +1769,11 @@ func (r *RuntimeResourceOverlay) GenerateAndroidBuildActions(ctx android.ModuleC
 	_, certificates := collectAppDeps(ctx, r, false, false)
 	certificates = processMainCert(r.ModuleBase, String(r.properties.Certificate), certificates, ctx)
 	signed := android.PathForModuleOut(ctx, "signed", r.Name()+".apk")
-	SignAppPackage(ctx, signed, r.aapt.exportPackage, certificates, nil)
+	var lineageFile android.Path
+	if lineage := String(r.properties.Lineage); lineage != "" {
+		lineageFile = android.PathForModuleSrc(ctx, lineage)
+	}
+	SignAppPackage(ctx, signed, r.aapt.exportPackage, certificates, lineageFile)
 	r.certificate = certificates[0]
 
 	r.outputFile = signed
