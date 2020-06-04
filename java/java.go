b0dc851ff4eca650042b52ce4ff06cb7e0312b77
Ulyana Trafimovich <skvadrik@google.com>
2020-06-04 10:37:36 +0000

    Revert "Rename DexJar interface method to DexJarBuildPath."
    This reverts commit 562c240185dd4adcbdb75d2924c69657f6ac7cff.
    
    Reason for revert: breaks `lunch full-eng && m checkbuild`.
    
    Change-Id: Id7c7d6240d98afaf8edd49b6c96cd05534b784cc

--- java/java.go
+++ java/java.go
@@ -498,7 +498,7 @@ type Dependency interface {
 	ImplementationJars() android.Paths
 	ResourceJars() android.Paths
 	ImplementationAndResourcesJars() android.Paths
-	DexJarBuildPath() android.Path
+	DexJar() android.Path
 	AidlIncludeDirs() android.Paths
 	ExportedSdkLibs() []string
 	ExportedPlugins() (android.Paths, []string)
@@ -1736,7 +1736,7 @@ func (j *Module) ImplementationJars() android.Paths {
 	return android.Paths{j.implementationJarFile}
 }
 
-func (j *Module) DexJarBuildPath() android.Path {
+func (j *Module) DexJar() android.Path {
 	return j.dexJarFile
 }
 
@@ -2562,7 +2562,7 @@ func (j *Import) ImplementationAndResourcesJars() android.Paths {
 	return android.Paths{j.combinedClasspathFile}
 }
 
-func (j *Import) DexJarBuildPath() android.Path {
+func (j *Import) DexJar() android.Path {
 	return nil
 }
 
@@ -2745,7 +2745,7 @@ func (j *DexImport) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 		j.Stem()+".jar", dexOutputFile)
 }
 
-func (j *DexImport) DexJarBuildPath() android.Path {
+func (j *DexImport) DexJar() android.Path {
 	return j.dexJarFile
 }
 
