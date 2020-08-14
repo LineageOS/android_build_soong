3a9d80a60cf85db6c50400beea87bc4dc1636fba
Jeff Vander Stoep <jeffv@google.com>
2020-08-14 13:53:14 +0200

    Rust: use 1.45.2 prebuilts
    Test: TH
    Change-Id: I2eab5af0a822673ca1f5bb2f63c88edc2c7830ff

--- rust/config/global.go
+++ rust/config/global.go
@@ -24,7 +24,7 @@ import (
 var pctx = android.NewPackageContext("android/soong/rust/config")
 
 var (
-	RustDefaultVersion = "1.44.0"
+	RustDefaultVersion = "1.45.2"
 	RustDefaultBase    = "prebuilts/rust/"
 	DefaultEdition     = "2018"
 	Stdlibs            = []string{
