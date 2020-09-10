5989a5753335acffaec3fb0b176e2baf08841d52
Jan Altensen <info@stricted.net>
2020-09-10 15:21:40 +0200

    soong: allow overriding header files
    Change-Id: Ia9d2210605c5927b529fbe9485b0e5abd079f487

--- android/variable.go
+++ android/variable.go
@@ -283,6 +283,8 @@ type productVariables struct {
 
 	DeviceKernelHeaders []string `json:",omitempty"`
 
+	TargetSpecificHeaderPath *string `json:",omitempty"`
+
 	ExtraVndkVersions []string `json:",omitempty"`
 
 	NamespacesToExport []string `json:",omitempty"`
