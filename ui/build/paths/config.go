9d1407d23e61f1b9d72df30fb7462ddbecb25229
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-09-12 18:11:56 +0200

    soong: Whitelist brotli
    Change-Id: I06d255b9098dcf4189e7fc3c163c5f6a2ebc0dc2

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -75,6 +75,7 @@ func GetConfig(name string) PathConfig {
 
 var Configuration = map[string]PathConfig{
 	"bash":    Allowed,
+	"brotli":  Allowed,
 	"dd":      Allowed,
 	"diff":    Allowed,
 	"dlv":     Allowed,
