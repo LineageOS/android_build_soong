3cd005d347b060f873b675e7a09e2aeb0e93eb25
Mohammad Samiul Islam <samiul@google.com>
2020-11-26 13:32:26 +0000

    Enable soong build tool to handle APEX compression
    1. Soong can now detect PRODUCT_COMPRESSED_APEX flag
         We don't want APEX to be compressed on all devices. Only those that
         have explicitely set PRODUCT_COMPRESSED_APEX flag.
    2. Handle "compressible" field in soong build rule
         On devices that supports APEX compression, all APEX will be
         compressed by default. If any apex does not want to be compressed,
         they will need to state that by setting "compressible" field to
         false
    3. Can use apex_compression_tool to compress APEX
         Note we compress the APEX after it has been signed. That way, when
         we decompress we will get a signed APEX.
    4. Place the compressed APEX in system with .capex extension
         This makes it easy to identify. We still preserve the original
         extension so that when we decompress, we can just rename by cuttif
         off the .capex extension.
    
    Note: with this change, we can create a system image with compressed
    APEX, but we cannot boot with it since platform doesn't know how to
    handle .capex files. Platform support will be added on follow up CLs.
    
    Bug: 172911362
    Test: OVERRIDE_PRODUCT_COMPRESSED_APEX=true m (apex_test.go)
    Test: observed $OUT/system/apex has .capex files
    Change-Id: I20ac4c4ceb521924c751a6017f979b2d808fdded

--- apex/builder.go
+++ apex/builder.go
@@ -66,6 +66,7 @@ func init() {
 	pctx.HostBinToolVariable("extract_apks", "extract_apks")
 	pctx.HostBinToolVariable("make_f2fs", "make_f2fs")
 	pctx.HostBinToolVariable("sload_f2fs", "sload_f2fs")
+	pctx.HostBinToolVariable("apex_compression_tool", "apex_compression_tool")
 	pctx.SourcePathVariable("genNdkUsedbyApexPath", "build/soong/scripts/gen_ndk_usedby_apex.sh")
 }
 
@@ -738,7 +739,7 @@ func (a *apexBundle) buildUnflattenedApex(ctx android.ModuleContext) {
 
 	////////////////////////////////////////////////////////////////////////////////////
 	// Step 4: Sign the APEX using signapk
-	a.outputFile = android.PathForModuleOut(ctx, a.Name()+suffix)
+	signedOutputFile := android.PathForModuleOut(ctx, a.Name()+suffix)
 
 	pem, key := a.getCertificateAndPrivateKey(ctx)
 	rule := java.Signapk
@@ -750,16 +751,47 @@ func (a *apexBundle) buildUnflattenedApex(ctx android.ModuleContext) {
 	if ctx.Config().UseRBE() && ctx.Config().IsEnvTrue("RBE_SIGNAPK") {
 		rule = java.SignapkRE
 		args["implicits"] = strings.Join(implicits.Strings(), ",")
-		args["outCommaList"] = a.outputFile.String()
+		args["outCommaList"] = signedOutputFile.String()
 	}
 	ctx.Build(pctx, android.BuildParams{
 		Rule:        rule,
 		Description: "signapk",
-		Output:      a.outputFile,
+		Output:      signedOutputFile,
 		Input:       unsignedOutputFile,
 		Implicits:   implicits,
 		Args:        args,
 	})
+	a.outputFile = signedOutputFile
+
+	// Process APEX compression if enabled
+	compressionEnabled := ctx.Config().CompressedApex() && proptools.BoolDefault(a.properties.Compressible, true)
+	if compressionEnabled && apexType == imageApex {
+		a.isCompressed = true
+		unsignedCompressedOutputFile := android.PathForModuleOut(ctx, a.Name()+".capex.unsigned")
+
+		compressRule := android.NewRuleBuilder(pctx, ctx)
+		compressRule.Command().
+			Text("rm").
+			FlagWithOutput("-f ", unsignedCompressedOutputFile)
+		compressRule.Command().
+			BuiltTool("apex_compression_tool").
+			Flag("compress").
+			FlagWithArg("--apex_compression_tool ", outHostBinDir+":"+prebuiltSdkToolsBinDir).
+			FlagWithInput("--input ", signedOutputFile).
+			FlagWithOutput("--output ", unsignedCompressedOutputFile)
+		compressRule.Build("compressRule", "Generate unsigned compressed APEX file")
+
+		signedCompressedOutputFile := android.PathForModuleOut(ctx, a.Name()+".capex")
+		ctx.Build(pctx, android.BuildParams{
+			Rule:        rule,
+			Description: "sign compressedApex",
+			Output:      signedCompressedOutputFile,
+			Input:       unsignedCompressedOutputFile,
+			Implicits:   implicits,
+			Args:        args,
+		})
+		a.outputFile = signedCompressedOutputFile
+	}
 
 	// Install to $OUT/soong/{target,host}/.../apex
 	if a.installable() {
