c297197e730c61717e5da087d133de72691a14fb
Julian Veit <claymore1298@gmail.com>
2021-05-04 07:57:50 +0200

    installclean_files: replace *.zip.md5sum with *.zip.sha256sum
    Needed after switching over from md5 to sha256.
    
    Change-Id: I91f92349423eeb99e7e4521f6239baabe8b1314c

--- ui/build/cleanbuild.go
+++ ui/build/cleanbuild.go
@@ -103,7 +103,7 @@ func installClean(ctx Context, config Config, what int) {
 		hostOut("vts-core"),
 		productOut("*.img"),
 		productOut("*.zip"),
-		productOut("*.zip.md5sum"),
+		productOut("*.zip.sha256sum"),
 		productOut("android-info.txt"),
 		productOut("apex"),
 		productOut("kernel"),
