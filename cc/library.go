9c993544650a3b60e3d4f819aa800f2737a36db2
Elvis Chien <elvis.chien@mediatek.com>
2021-06-25 01:15:17 +0800

    Allow PRODUCT_CFI_INCLUDE_PATHS to work with blueprints also
    but disable it for host modules like how it's done for Android.mk
    
    Bug:
    179233410
    
    Test:
    m -j32
    
    Change-Id: I246cd9163e06997a2b50cd25688370690c8929af

--- cc/library.go
+++ cc/library.go
@@ -1751,7 +1751,7 @@ func (library *libraryDecorator) install(ctx ModuleContext, file android.Path) {
 					mayUseCoreVariant = false
 				}
 
-				if ctx.Config().CFIEnabledForPath(ctx.ModuleDir()) && ctx.Arch().ArchType == android.Arm64 {
+				if ctx.Config().CFIEnabledForPath(ctx.ModuleDir()) {
 					mayUseCoreVariant = false
 				}
 
