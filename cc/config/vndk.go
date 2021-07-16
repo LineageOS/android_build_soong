50ef6e204ac416c9086b42731631a73b699ccb81
Chris Ye <lzye@google.com>
2021-07-16 10:40:46 -0700

    Add android.hardware.wifi.hostapd-V1-ndk_platform to VNDK list.
    Bug: 194806512
    Test: Boot up.
    Change-Id: I50518b0291dd40dc051c340faa2641c4cf5952d4

--- cc/config/vndk.go
+++ cc/config/vndk.go
@@ -74,6 +74,8 @@ var VndkMustUseVendorVariantList = []string{
 	"android.hardware.weaver-ndk_platform",
 	"android.hardware.weaver-unstable-ndk_platform",
 	"android.system.keystore2-V1-ndk",
+	"android.hardware.wifi.hostapd-V1-ndk",
+	"android.hardware.wifi.hostapd-V1-ndk_platform",
 	"android.system.keystore2-V1-ndk_platform",
 	"android.system.keystore2-ndk_platform",
 	"android.system.keystore2-unstable-ndk_platform",
