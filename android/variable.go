a10ef270eb096b9035e47d0487d85347a6d9a873
Inseob Kim <inseob@google.com>
2021-09-15 03:04:53 +0000

    Add more sepolicy variables to soong_config
    Bug: 33691272
    Test: m selinux_policy on sc-dev
    Change-Id: I4a7288d5f4ff99233fbdb56a8fd92da2d6f2ef7d

--- android/variable.go
+++ android/variable.go
@@ -344,6 +344,7 @@ type productVariables struct {
 
 	BoardSepolicyVers       *string `json:",omitempty"`
 	PlatformSepolicyVersion *string `json:",omitempty"`
+	TotSepolicyVersion      *string `json:",omitempty"`
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
 
@@ -407,6 +408,9 @@ type productVariables struct {
 	SelinuxIgnoreNeverallows bool `json:",omitempty"`
 
 	SepolicySplit bool `json:",omitempty"`
+
+	SepolicyFreezeTestExtraDirs         []string `json:",omitempty"`
+	SepolicyFreezeTestExtraPrebuiltDirs []string `json:",omitempty"`
 }
 
 func boolPtr(v bool) *bool {
