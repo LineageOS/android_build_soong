01812020c1e4c00e6d73a93bac84f1cabc540ce1
Jingwen Chen <jingwen@google.com>
2021-11-19 14:29:43 +0000

    Add support for writing all Soong config variables into @soong_injection.
    Also remove the need to use bp2build_available on
    soong_config_module_types as we want to convert every single of them
    into the tree for a complete soong_injection soong_config_variables.bzl
    file.
    
    The variables are split into their bool, value and string types
    respectively, as they all need to be handled differently on the Bazel
    product_platform side, as well as for generating constraint values and
    settings. For example, value variables need to integrate with
    TemplateVariableInfo, and string variables need to include the string
    value itself into the select key/constraint value.
    
    Sample soong_config_variables.bzl file: https://gist.github.com/jin/cef700bfb20c8656a931306dd71d47e1
    
    Test: CI
    Bug: 198556411
    Change-Id: I8665dd1269a507edb37de62407ed3641564bea5c

--- bp2build/conversion.go
+++ bp2build/conversion.go
@@ -25,6 +25,8 @@ func CreateSoongInjectionFiles(cfg android.Config, metrics CodegenMetrics) []Baz
 
 	files = append(files, newFile("metrics", "converted_modules.txt", strings.Join(metrics.convertedModules, "\n")))
 
+	files = append(files, newFile("product_config", "soong_config_variables.bzl", cfg.Bp2buildSoongConfigDefinitions.String()))
+
 	return files
 }
 
