5ded619f3ea6d8f489b3e862e34ca2b1d413bd3d
Yi Kong <yikong@google.com>
2021-12-06 15:43:06 +0800

    addSamplingProfileGatherFlags is no longer needed
    We already add the "-fdebug-info-for-profiling" flag globally by default.
    Besides, Sampling PGO does not need additional target-specific build
    flags, unlike instrumentation PGO.
    
    This is preparation to separate out sampling pgo into a separate module.
    NFCI.
    
    Test: presubmit
    Change-Id: Icb28a98c021a78db25d8cb5ca9694ed6b7367a8d

--- android/config.go
+++ android/config.go
@@ -1194,10 +1194,6 @@ func (c *deviceConfig) DeviceKernelHeaderDirs() []string {
 	return c.config.productVariables.DeviceKernelHeaders
 }
 
-func (c *deviceConfig) SamplingPGO() bool {
-	return Bool(c.config.productVariables.SamplingPGO)
-}
-
 // JavaCoverageEnabledForPath returns whether Java code coverage is enabled for
 // path. Coverage is enabled by default when the product variable
 // JavaCoveragePaths is empty. If JavaCoveragePaths is not empty, coverage is
