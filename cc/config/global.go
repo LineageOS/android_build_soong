d950ab8bb3a5c9ddd98875ae8e1820a49d964d88
Pirama Arumuga Nainar <pirama@google.com>
2022-01-26 13:48:22 -0800

    Switch to clang-r445002
    - Disable -Wbitwise-instead-of-logical for external projects.
    
    Test: N/A
    Change-Id: Icb081bf6f17fcd5bf9759efa99fd26413c74f7c3

--- cc/config/global.go
+++ cc/config/global.go
@@ -236,6 +236,8 @@ var (
 		// http://b/197240255
 		"-Wno-unused-but-set-variable",
 		"-Wno-unused-but-set-parameter",
+		// http://b/215753485
+		"-Wno-bitwise-instead-of-logical",
 	}
 
 	// Extra cflags for external third-party projects to disable warnings that
@@ -280,8 +282,8 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r437112b"
-	ClangDefaultShortVersion = "14.0.1"
+	ClangDefaultVersion      = "clang-r445002"
+	ClangDefaultShortVersion = "14.0.2"
 
 	// Directories with warnings from Android.bp files.
 	WarningAllowedProjects = []string{
