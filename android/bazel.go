9dfb139b6d0bffe19a4ee705622468c3b404e63e
Sam Delmerico <delmerico@google.com>
2022-02-10 21:11:59 +0000

    convert android_app certificate property, bp2build
    The android_app certificate property can be converted to the
    @android_rules debug_signing_keys attribute in Bazel after converting
    the .pk8/.pem key pairs to a JKS keystore in Starlark.
    
    Test: b build -s //frameworks/base/tests/appwidgets/AppWidgetHostTest
      and verify includes SignApk action with generated keystore
    Bug: 194133023
    Change-Id: I2c4276f94a7856fc68a7674e89742f887dca31b4

--- android/bazel.go
+++ android/bazel.go
@@ -227,6 +227,7 @@ var (
 
 		"prebuilts/gcc":/* recursive = */ true,
 		"prebuilts/build-tools":/* recursive = */ false,
+		"prebuilts/jdk/jdk11":/* recursive = */ false,
 		"prebuilts/sdk":/* recursive = */ false,
 		"prebuilts/sdk/current/extras/app-toolkit":/* recursive = */ false,
 		"prebuilts/sdk/current/support":/* recursive = */ false,
@@ -245,6 +246,7 @@ var (
 		"build/bazel/examples/soong_config_variables":        Bp2BuildDefaultTrueRecursively,
 		"build/bazel/examples/apex/minimal":                  Bp2BuildDefaultTrueRecursively,
 		"build/make/tools/signapk":                           Bp2BuildDefaultTrue,
+		"build/make/target/product/security":                 Bp2BuildDefaultTrue,
 		"build/soong":                                        Bp2BuildDefaultTrue,
 		"build/soong/cc/libbuildversion":                     Bp2BuildDefaultTrue, // Skip tests subdir
 		"build/soong/cc/ndkstubgen":                          Bp2BuildDefaultTrue,
@@ -325,6 +327,7 @@ var (
 		"external/zstd":                                      Bp2BuildDefaultTrueRecursively,
 		"frameworks/base/media/tests/MediaDump":              Bp2BuildDefaultTrue,
 		"frameworks/base/startop/apps/test":                  Bp2BuildDefaultTrue,
+		"frameworks/base/tests/appwidgets/AppWidgetHostTest": Bp2BuildDefaultTrueRecursively,
 		"frameworks/native/libs/adbd_auth":                   Bp2BuildDefaultTrueRecursively,
 		"frameworks/native/opengl/tests/gl2_cameraeye":       Bp2BuildDefaultTrue,
 		"frameworks/native/opengl/tests/gl2_java":            Bp2BuildDefaultTrue,
