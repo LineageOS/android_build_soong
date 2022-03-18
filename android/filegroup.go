0940b8993733ac56a13db6d3bb3a2a23d3aa84c8
Liz Kammer <eakammer@google.com>
2022-03-18 15:55:04 -0400

    Handle arch/cpu variants in mixed builds
    Test: mixed_libc.sh
    Change-Id: Ib5084060199b8e1a5955bd51bc3fd793bb83a1eb

--- android/filegroup.go
+++ android/filegroup.go
@@ -119,12 +119,12 @@ func (fg *fileGroup) maybeGenerateBazelBuildActions(ctx ModuleContext) {
 		return
 	}
 
-	archVariant := ctx.Arch().ArchType
+	archVariant := ctx.Arch().String()
 	osVariant := ctx.Os()
 	if len(fg.Srcs()) == 1 && fg.Srcs()[0].Base() == fg.Name() {
 		// This will be a regular file target, not filegroup, in Bazel.
 		// See FilegroupBp2Build for more information.
-		archVariant = Common
+		archVariant = Common.String()
 		osVariant = CommonOS
 	}
 
