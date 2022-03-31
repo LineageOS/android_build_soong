c15b9e99e5e208410a1f1c3766c97125e63c22b3
Paul Duffin <paulduffin@google.com>
2022-03-31 15:42:30 +0100

    bootclasspath_fragment: Add test specific module type
    This is needed to allow the behavior of the bootclasspath_fragment to
    be tweaked for test fragments.
    
    Bug: 194063708
    Test: m nothing
    Change-Id: Iee5c09d5b580d088ba081d95a788dbde883078ed

--- java/bootclasspath_fragment.go
+++ java/bootclasspath_fragment.go
@@ -42,6 +42,7 @@ func init() {
 
 func registerBootclasspathFragmentBuildComponents(ctx android.RegistrationContext) {
 	ctx.RegisterModuleType("bootclasspath_fragment", bootclasspathFragmentFactory)
+	ctx.RegisterModuleType("bootclasspath_fragment_test", testBootclasspathFragmentFactory)
 	ctx.RegisterModuleType("prebuilt_bootclasspath_fragment", prebuiltBootclasspathFragmentFactory)
 }
 
@@ -227,6 +228,9 @@ type BootclasspathFragmentModule struct {
 	android.SdkBase
 	ClasspathFragmentBase
 
+	// True if this fragment is for testing purposes.
+	testFragment bool
+
 	properties bootclasspathFragmentProperties
 
 	sourceOnlyProperties SourceOnlyBootclasspathProperties
@@ -295,6 +299,12 @@ func bootclasspathFragmentFactory() android.Module {
 		// Initialize the contents property from the image_name.
 		bootclasspathFragmentInitContentsFromImage(ctx, m)
 	})
+	return m
+}
+
+func testBootclasspathFragmentFactory() android.Module {
+	m := bootclasspathFragmentFactory().(*BootclasspathFragmentModule)
+	m.testFragment = true
 	return m
 }
 
@@ -813,6 +823,11 @@ func (b *BootclasspathFragmentModule) createHiddenAPIFlagInput(ctx android.Modul
 	input.DependencyStubDexJarsByScope.addStubDexJarsByModule(dependencyHiddenApiInfo.TransitiveStubDexJarsByScope)
 
 	return input
+}
+
+// isTestFragment returns true if the current module is a test bootclasspath_fragment.
+func (b *BootclasspathFragmentModule) isTestFragment() bool {
+	return b.testFragment
 }
 
 // produceHiddenAPIOutput produces the hidden API all-flags.csv file (and supporting files)
