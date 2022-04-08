2394224da3f3b71b81ff53758a5e41e872b99340
Liz Kammer <eakammer@google.com>
2022-04-08 15:41:00 -0400

    Remove version_selector mutator
    This mutator simply set a mutated property immediately before a separate
    mutator used the value in the property. Instead of a separate mutator,
    condense the two into a single mutator pass.
    
    Test: CI
    Change-Id: I26c392cdb96f6182a19fbcf0cf3f88a8fc72f81c

--- cc/cc.go
+++ cc/cc.go
@@ -48,7 +48,6 @@ func RegisterCCBuildComponents(ctx android.RegistrationContext) {
 		ctx.BottomUp("vndk", VndkMutator).Parallel()
 		ctx.BottomUp("link", LinkageMutator).Parallel()
 		ctx.BottomUp("test_per_src", TestPerSrcMutator).Parallel()
-		ctx.BottomUp("version_selector", versionSelectorMutator).Parallel()
 		ctx.BottomUp("version", versionMutator).Parallel()
 		ctx.BottomUp("begin", BeginMutator).Parallel()
 		ctx.BottomUp("sysprop_cc", SyspropMutator).Parallel()
@@ -2103,7 +2102,7 @@ func AddSharedLibDependenciesWithVersions(ctx android.BottomUpMutatorContext, mo
 
 	variations = append([]blueprint.Variation(nil), variations...)
 
-	if version != "" && CanBeOrLinkAgainstVersionVariants(mod) {
+	if version != "" && canBeOrLinkAgainstVersionVariants(mod) {
 		// Version is explicitly specified. i.e. libFoo#30
 		variations = append(variations, blueprint.Variation{Mutator: "version", Variation: version})
 		if tag, ok := depTag.(libraryDependencyTag); ok {
