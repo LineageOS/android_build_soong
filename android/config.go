b5f6d9ea511a6515637de0c4aaaf3859061f07cb
Alix <agespino@google.com>
2022-04-20 23:00:58 +0000

    Deprecating clang Property
    Clang has defaulted to true since 2018. Clang property is deprecated.
    Set up a BUILD_BROKEN_CLANG_PROPERTY flag so partners can bypass \
    errors from using clang.
    
    Bug: 208980553
    Test: m nothing & treehugger
    Change-Id: I8fd7d3e6bdbb963279248d3482e8a590238727e3

--- android/config.go
+++ android/config.go
@@ -1630,6 +1630,10 @@ func (c *deviceConfig) ShippingApiLevel() ApiLevel {
 	return uncheckedFinalApiLevel(apiLevel)
 }
 
+func (c *deviceConfig) BuildBrokenClangProperty() bool {
+	return c.config.productVariables.BuildBrokenClangProperty
+}
+
 func (c *deviceConfig) BuildBrokenEnforceSyspropOwner() bool {
 	return c.config.productVariables.BuildBrokenEnforceSyspropOwner
 }
