877d5ee596a517b8094f11c3689ba257232c938a
Bob Badour <bbadour@google.com>
2022-05-27 19:42:16 -0700

    Tool to create license metadata for copied target.
    Bug: 213388645
    Bug: 240342946
    
    Test: m copy_license_metadata
    Test: m cts dist reportmissinglicenses
    
    Change-Id: I75c196ceca221effb2d6c972e250f5d42854e42f
    Merged-in: I75c196ceca221effb2d6c972e250f5d42854e42f

--- android/licenses.go
+++ android/licenses.go
@@ -331,6 +331,8 @@ func init() {
 func licensesMakeVarsProvider(ctx MakeVarsContext) {
 	ctx.Strict("BUILD_LICENSE_METADATA",
 		ctx.Config().HostToolPath(ctx, "build_license_metadata").String())
+	ctx.Strict("COPY_LICENSE_METADATA",
+		ctx.Config().HostToolPath(ctx, "copy_license_metadata").String())
 	ctx.Strict("HTMLNOTICE", ctx.Config().HostToolPath(ctx, "htmlnotice").String())
 	ctx.Strict("XMLNOTICE", ctx.Config().HostToolPath(ctx, "xmlnotice").String())
 	ctx.Strict("TEXTNOTICE", ctx.Config().HostToolPath(ctx, "textnotice").String())
