39cadf9f067d7e87c463434c5e4f2b4152810384
Jooyung Han <jooyung@google.com>
2022-07-25 12:32:32 +0900

    Allow libwifi-hal to use makefile_goal
    To migrate libwifi-hal from make to soong, its dependencis
    (libwifi-hal-<vendor>) are wrapped in makefile_goal until they are
    migrated to soong as well.
    
    Bug: 239984067
    Test: m
    Merged-In: Icb09ef1bf40311498db04b7358634eadd0fed5fa
    Change-Id: Icb09ef1bf40311498db04b7358634eadd0fed5fa
    (cherry picked from commit f9392095d7e6f065310ab7662bb34897295f7d9f)

--- android/neverallow_test.go
+++ android/neverallow_test.go
@@ -324,7 +324,32 @@ var neverallowTests = []struct {
 			`),
 		},
 		expectedErrors: []string{
-			"Only boot images may be imported as a makefile goal.",
+			"Only boot images.* may be imported as a makefile goal",
+		},
+	},
+	{
+		name: "disallowed makefile_goal outside external",
+		fs: map[string][]byte{
+			"project/Android.bp": []byte(`
+				makefile_goal {
+					name: "foo",
+					product_out_path: "obj/EXE/foo",
+				}
+			`),
+		},
+		expectedErrors: []string{
+			"not in allowed projects",
+		},
+	},
+	{
+		name: "allow makefile_goal within external",
+		fs: map[string][]byte{
+			"frameworks/opt/net/wifi/libwifi_hal/Android.bp": []byte(`
+				makefile_goal {
+					name: "foo",
+					product_out_path: "obj/EXE/foo",
+				}
+			`),
 		},
 	},
 	// Tests for the rule prohibiting the use of framework
