92e92e49cafdbfe0d07af1ffd8b096a03db2ff3c
Cole Faust <colefaust@google.com>
2022-08-26 16:12:42 -0700

    Allowlist libyuv
    It's a dependency of media.swcodec
    
    Bug: 237041122
    Test: ./build/bazel/ci/bp2build.sh
    Change-Id: I17e512af7c0d7d9ded7311ad61cd339b380fbef0

--- android/allowlists/allowlists.go
+++ android/allowlists/allowlists.go
@@ -137,6 +137,7 @@ var (
 		"external/libjpeg-turbo":                 Bp2BuildDefaultTrueRecursively,
 		"external/libmpeg2":                      Bp2BuildDefaultTrueRecursively,
 		"external/libpng":                        Bp2BuildDefaultTrueRecursively,
+		"external/libyuv":                        Bp2BuildDefaultTrueRecursively,
 		"external/lz4/lib":                       Bp2BuildDefaultTrue,
 		"external/lzma/C":                        Bp2BuildDefaultTrueRecursively,
 		"external/mdnsresponder":                 Bp2BuildDefaultTrueRecursively,
