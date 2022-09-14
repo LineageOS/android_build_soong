d0fe1307aafc7714f3f9e9949a2c143be04fcf44
Paul Duffin <paulduffin@google.com>
2022-09-14 17:04:51 +0000

    Revert "bootclasspath_fragment: Treat some specific modules as test"
    This reverts commit ff9b6faba2b9a949289c70657db87c0b1790cc07.
    
    Reason for revert: No longer needed
    Bug: 194063708
    Test: m nothing
    Change-Id: Ic2f6761c9a0a00a612724cb429f002a16522e53f

--- java/bootclasspath_fragment.go
+++ java/bootclasspath_fragment.go
@@ -827,22 +827,7 @@ func (b *BootclasspathFragmentModule) createHiddenAPIFlagInput(ctx android.Modul
 
 // isTestFragment returns true if the current module is a test bootclasspath_fragment.
 func (b *BootclasspathFragmentModule) isTestFragment() bool {
-	if b.testFragment {
-		return true
-	}
-
-	// TODO(b/194063708): Once test fragments all use bootclasspath_fragment_test
-	// Some temporary exceptions until all test fragments use the
-	// bootclasspath_fragment_test module type.
-	name := b.BaseModuleName()
-	if strings.HasPrefix(name, "test_") {
-		return true
-	}
-	if name == "apex.apexd_test_bootclasspath-fragment" {
-		return true
-	}
-
-	return false
+	return b.testFragment
 }
 
 // produceHiddenAPIOutput produces the hidden API all-flags.csv file (and supporting files)
