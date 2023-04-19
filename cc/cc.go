1db4348734acfd0b4a38e2ff49dca2df4bac0d84
Usta (Tsering) Shrestha <usta@google.com>
2023-04-19 14:02:52 +0000

    Revert "mixed builds correctly reference stubs libs"
    This reverts commit ca438e6b72aba8306da500b9426acad1aa767b53.
    
    Reason for revert: ci post-submit failures in aosp-master-bazel of targets mixed-droid-clean and mixed-droid-incremental:
    FAILED: ninja: 'out/target/product/generic_arm64/obj/SHARED_LIBRARIES/libc_intermediates/libc.so.toc', needed by 'out/target/product/generic_arm64/obj/EXECUTABLES/updater_intermediates/LINKED/updater', missing and no known rule to make it
    12:21:27 ninja failed with: exit status 1
    
    Change-Id: I081b499d23f2568cdf6227c4e3b0278164086b69

--- cc/cc.go
+++ cc/cc.go
@@ -1960,17 +1960,6 @@ func (c *Module) ProcessBazelQueryResponse(ctx android.ModuleContext) {
 	c.maybeInstall(mctx, apexInfo)
 }
 
-func moduleContextFromAndroidModuleContext(actx android.ModuleContext, c *Module) ModuleContext {
-	ctx := &moduleContext{
-		ModuleContext: actx,
-		moduleContextImpl: moduleContextImpl{
-			mod: c,
-		},
-	}
-	ctx.ctx = ctx
-	return ctx
-}
-
 func (c *Module) GenerateAndroidBuildActions(actx android.ModuleContext) {
 	// Handle the case of a test module split by `test_per_src` mutator.
 	//
@@ -1990,7 +1979,13 @@ func (c *Module) GenerateAndroidBuildActions(actx android.ModuleContext) {
 
 	c.makeLinkType = GetMakeLinkType(actx, c)
 
-	ctx := moduleContextFromAndroidModuleContext(actx, c)
+	ctx := &moduleContext{
+		ModuleContext: actx,
+		moduleContextImpl: moduleContextImpl{
+			mod: c,
+		},
+	}
+	ctx.ctx = ctx
 
 	deps := c.depsToPaths(ctx)
 	if ctx.Failed() {
