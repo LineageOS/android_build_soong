48203e3dee2be5a5432af1b76be6a3b58ca02933
Jiakai Zhang <jiakaiz@google.com>
2023-06-02 23:42:21 +0100

    Add an allowlist for modules that have broken optional_uses_libs.
    Bug: 282877248
    Test: m nothing
    Change-Id: I1afe9d523af0633f0428d4c15e1417db1dae4e93

--- android/config.go
+++ android/config.go
@@ -1925,6 +1925,10 @@ func (c *deviceConfig) BuildBrokenInputDir(name string) bool {
 	return InList(name, c.config.productVariables.BuildBrokenInputDirModules)
 }
 
+func (c *config) BuildWarningBadOptionalUsesLibsAllowlist() []string {
+	return c.productVariables.BuildWarningBadOptionalUsesLibsAllowlist
+}
+
 func (c *deviceConfig) GenruleSandboxing() bool {
 	return Bool(c.config.productVariables.GenruleSandboxing)
 }
