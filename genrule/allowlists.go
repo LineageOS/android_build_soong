ff833a95e8896a3f98ca3a0e3c9a4b3c7dfcb702
David Duarte <licorne@google.com>
2023-06-14 00:07:37 +0000

    Remove pandora-python-gen-src from SandboxingDenyModuleList
    Test: Manual diff, see linked commit
    Change-Id: Icec1edd6a4420377dd79fc736bea77910daad1b9

--- genrule/allowlists.go
+++ genrule/allowlists.go
@@ -124,7 +124,6 @@ var (
 		"nos_app_weaver_service_genc++_headers",
 		"nos_app_weaver_service_genc++_mock",
 		"nos_generator_test_service_genc++",
-		"pandora-python-gen-src",
 	}
 
 	SandboxingDenyPathList = []string{
