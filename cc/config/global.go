ed7c73edfede577c0f642c662f2c5b6716ffb7c6
Yi Kong <yikong@google.com>
2023-08-25 15:18:48 +0900

    Update clang version to clang-r498229b
    Now built with MLGO. This reduces the total Clang invocation time for
    building AOSP by 0.7%.
    
        TOOLCHAIN_RUSAGE_OUTPUT=/tmp/rusage.txt m -j32
        PGO + LTO + BOLT          83839.78s
        MLGO + PGO + LTO + BOLT   82949.30s
    
    Test: presubmit
    Bug: 293616148
    Change-Id: I5364c341520172e8e1acafb4aa3330922805ac0e

--- cc/config/global.go
+++ cc/config/global.go
@@ -320,7 +320,7 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r498229"
+	ClangDefaultVersion      = "clang-r498229b"
 	ClangDefaultShortVersion = "17"
 
 	// Directories with warnings from Android.bp files.
