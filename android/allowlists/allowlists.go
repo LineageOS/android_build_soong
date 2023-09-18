01de3ac6197665eb8baa0295a344ebcfca5ea4c0
Liz Kammer <eakammer@google.com>
2023-09-18 13:39:06 -0400

    Add keep sorted tags
    Test: n/a
    Change-Id: Ibfb96b3e9c2f4707eb585714069bbb81671457b4

--- android/allowlists/allowlists.go
+++ android/allowlists/allowlists.go
@@ -918,6 +918,7 @@ var (
 	}
 
 	Bp2buildModuleTypeAlwaysConvertList = []string{
+		// go/keep-sorted start
 		"aconfig_declarations",
 		"aconfig_value_set",
 		"aconfig_values",
@@ -937,6 +938,7 @@ var (
 		"linker_config",
 		"sysprop_library",
 		"xsd_config",
+		// go/keep-sorted end
 	}
 
 	// Add the names of modules that bp2build should never convert, if it is
