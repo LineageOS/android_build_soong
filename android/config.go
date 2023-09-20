d91a8e806a832eb3d97862b9baf3fec24485a64a
Jihoon Kang <jihoonkang@google.com>
2023-09-20 00:54:47 +0000

    Add flagged api hide conditional to droidstubs
    APIs annotated with @FlaggedApi should not be included in the artifact
    when building sdk target products in the "next" release configuration.
    
    This change adds such logic by passing additional flag to metalava in
    droidstubs.
    
    The flag does not need to be passed to metalava invocation done in
    java_api_library, as java_api_library generates stubs using api
    signature files (i.e. *-current.txt files), and they will not contain
    apis marked @FlaggedApi. The metalava invocation in droidstubs is
    responsible for removing such apis.
    
    Test: go test ./java && m TARGET_PRODUCT=sdk TESTING_TARGET_RELEASE_NEXT=true nothing and inspect ninja command for generating stubs and verify the flag is included
    Bug: 299570421
    Change-Id: Ia4b699b6e3ff6324f050eecc9ff5b622fdc04621

--- android/config.go
+++ android/config.go
@@ -2088,3 +2088,7 @@ func (c *deviceConfig) NextReleaseHideFlaggedApi() bool {
 func (c *deviceConfig) ReleaseExposeFlaggedApi() bool {
 	return Bool(c.config.productVariables.ReleaseExposeFlaggedApi)
 }
+
+func (c *deviceConfig) HideFlaggedApis() bool {
+	return c.NextReleaseHideFlaggedApi() && !c.ReleaseExposeFlaggedApi()
+}
