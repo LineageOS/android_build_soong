5548bf305368571c40461a818f24f26cbe2de533
Maxim Siniavine <siniavine@google.com>
2023-09-28 01:01:59 +0000

    Revert "Add soong config variable Release_expose_flagged_api"
    Revert submission 2755310
    
    Reason for revert: Broken build b/302407753
    
    Reverted changes: /q/submissionid:2755310
    
    Change-Id: Ib5c01033882500296989a7c4476a723d95292aab

--- android/config.go
+++ android/config.go
@@ -2084,7 +2084,3 @@ func (c *deviceConfig) CheckVendorSeappViolations() bool {
 func (c *deviceConfig) NextReleaseHideFlaggedApi() bool {
 	return Bool(c.config.productVariables.NextReleaseHideFlaggedApi)
 }
-
-func (c *deviceConfig) ReleaseExposeFlaggedApi() bool {
-	return Bool(c.config.productVariables.ReleaseExposeFlaggedApi)
-}
