14d60d1fd0eb20fbf4f845803c9f488abfcf159e
Vinh Tran <vinhdaitran@google.com>
2023-10-11 12:17:30 -0400

    Allowlist toolchain/pgo-profiles
    Test: m bp2build
    Change-Id: I3c4b383941c24aa948e1a42aa782349567398c5f

--- android/allowlists/allowlists.go
+++ android/allowlists/allowlists.go
@@ -453,6 +453,7 @@ var (
 
 		"test/vts/vts_hal_hidl_target": Bp2BuildDefaultTrueRecursively,
 
+		"toolchain/pgo-profiles":                      Bp2BuildDefaultTrueRecursively,
 		"tools/apifinder":                             Bp2BuildDefaultTrue,
 		"tools/apksig":                                Bp2BuildDefaultTrue,
 		"tools/dexter/slicer":                         Bp2BuildDefaultTrueRecursively,
