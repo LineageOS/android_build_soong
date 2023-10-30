b6d6fc9c63024ccf48dfb578e064899bd464211b
Chris Parsons <cparsons@google.com>
2023-10-30 16:21:06 +0000

    Delete bazel-related files while bazel is off
    Bug: 189485959
    Test: Manually verified that files are removed
    Test: Presubmits
    Change-Id: Ia279937d6d335174776ff661ab20c7821b979ca7

--- ui/build/soong.go
+++ ui/build/soong.go
@@ -631,6 +631,10 @@ func runSoong(ctx Context, config Config) {
 
 		if config.BazelBuildEnabled() || config.Bp2Build() {
 			checkEnvironmentFile(ctx, soongBuildEnv, config.UsedEnvFile(bp2buildFilesTag))
+		} else {
+			// Remove bazel files in the event that bazel is disabled for the build.
+			// These files may have been left over from a previous bazel-enabled build.
+			cleanBazelFiles(config)
 		}
 
 		if config.JsonModuleGraph() {
@@ -773,6 +777,18 @@ func loadSoongBuildMetrics(ctx Context, config Config, oldTimestamp time.Time) {
 		}
 		ctx.Tracer.Complete(desc, ctx.Thread,
 			event.GetStartTime(), event.GetStartTime()+event.GetRealTime())
+	}
+}
+
+func cleanBazelFiles(config Config) {
+	files := []string{
+		shared.JoinPath(config.SoongOutDir(), "bp2build"),
+		shared.JoinPath(config.SoongOutDir(), "workspace"),
+		shared.JoinPath(config.SoongOutDir(), bazel.SoongInjectionDirName),
+		shared.JoinPath(config.OutDir(), "bazel")}
+
+	for _, f := range files {
+		os.RemoveAll(f)
 	}
 }
 
