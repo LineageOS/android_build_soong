6592e87dbfe1e49b5c57ca8ead0bb48e6354e261
Jihoon Kang <jihoonkang@google.com>
2023-12-19 01:13:16 +0000

    Add aconfig_declarations property to droidstubs and java_sdk_library
    In consideration of the incremental build performance, this change let
    droidstubs and java_sdk_library (which generates droidstubs per api
    scope) modules to specify `aconfig_declaration` modules where the
    dependent flags are defined in via the "aconfig_declarations" property,
    opposed to passing uniform "all_aconfig_declaration"-generated flag
    arguments to metalava.
    
    When "aconfig_declarations" property is defined for java_sdk_library
    modules, the property is passed to the generated droidstubs modules.
    
    When "aconfig_declarations" property is defined for droidstubs modules,
    the all aconfig_declaration modules listed in the property are listed as
    deps, all cache protobuf files are gathered and metalava-consumable
    flags are generated in "revert-annotations.txt".
    
    Although this change introduces scalable implementation to easily
    support generation of the "runtime" stubs corresponding flags, actual
    support of the runtime flags/stubs will be done in future changes. This
    change mostly focuses on the generation of the "exportable" flags.
    
    Utilization of the generated "exportable" flags will be done in future
    changes.
    
    Test: go test ./java
    Bug: 315485740
    Change-Id: I37becd1b9dd9069d7ac4abed130906df30b3fdf4

--- java/droiddoc.go
+++ java/droiddoc.go
@@ -397,6 +397,15 @@ func (j *Javadoc) collectDeps(ctx android.ModuleContext) deps {
 			sm := module.(SystemModulesProvider)
 			outputDir, outputDeps := sm.OutputDirAndDeps()
 			deps.systemModules = &systemModules{outputDir, outputDeps}
+		case aconfigDeclarationTag:
+			if dep, ok := android.OtherModuleProvider(ctx, module, android.AconfigDeclarationsProviderKey); ok {
+				deps.aconfigProtoFiles = append(deps.aconfigProtoFiles, dep.IntermediateCacheOutputPath)
+			} else {
+				ctx.ModuleErrorf("Only aconfig_declarations module type is allowed for "+
+					"flags_packages property, but %s is not aconfig_declarations module type",
+					module.Name(),
+				)
+			}
 		}
 	})
 	// do not pass exclude_srcs directly when expanding srcFiles since exclude_srcs
