c87e30737f77a7afbb1b31b80b4fabd8ea5cfe0c
DroidFreak32 <rushabshah32@gmail.com>
2024-02-13 10:36:21 +0530

    DNM: Allow using host zstd
    Change-Id: I68e064e4e57271265c391028c7a990cb1abe604f

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -107,6 +107,7 @@ var Configuration = map[string]PathConfig{
 	"tr":      Allowed,
 	"unzip":   Allowed,
 	"zip":     Allowed,
+	"zstd":    Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
