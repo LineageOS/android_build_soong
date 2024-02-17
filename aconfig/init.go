bba555ee319f8c961bab4fc288d08c9274eed3c9
Yu Liu <yudiliu@google.com>
2024-02-17 00:36:42 +0000

    Support aconfig stoage files for apex.
    Bug: 323071835
    Test: unit tests and manual build test.
    Change-Id: I4a1a05b3afdf88058a970040b3f176f36c5c8037

--- aconfig/init.go
+++ aconfig/init.go
@@ -76,6 +76,14 @@ var (
 			},
 		}, "cache_files")
 
+	CreateStorageRule = pctx.AndroidStaticRule("aconfig_create_storage",
+		blueprint.RuleParams{
+			Command: `${aconfig} create-storage --container ${container} --file ${file_type} --out ${out} ${cache_files}`,
+			CommandDeps: []string{
+				"${aconfig}",
+			},
+		}, "container", "file_type", "cache_files")
+
 	// For exported_java_aconfig_library: Generate a JAR from all
 	// java_aconfig_libraries to be consumed by apps built outside the
 	// platform
