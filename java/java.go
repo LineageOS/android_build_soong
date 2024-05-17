25b9222a43a599cccbce856bb72f4190059a30c8
Jiyong Park <jiyong@google.com>
2024-05-17 22:58:54 +0000

    Revert^2 "Install jni symlinks in Soong"
    b7646e4d4ff20002c658ab667b4c1892bc3f0f1d
    
    This is a relanding of I0930cb1ebb8ca8a6efd64b1ce2cdfd1c47fe19ef plus
    some forward fix described below:
    
    Export non-embedded JNI lib names via LOCAL_REQUIRED_MODULES
    
    The non-embedded JNI libs are installed as the dependencies of the APK.
    However, that dependency is not revealed to the Make world and as a
    result, the JNI libs are dropped from the file_list.txt file which Make
    uses to filter files to include in the image file.
    
    Adding the lib names to LOCAL_REQUIRED_MODULES fixes it.
    
    Bug: 341335305
    Bug: 330276359
    Test: m out/target/product/vsoc_x86_64_only/obj/PACKAGING/system_intermediates/file_list.txt
    and check if libcarservicejni.so is there
    Test: go test ./... under soong/java
    
    Change-Id: If915a05909129c92fab7a6cbbd0c4c55f5ced598

--- java/java.go
+++ java/java.go
@@ -491,6 +491,7 @@ type jniLib struct {
 	coverageFile   android.OptionalPath
 	unstrippedFile android.Path
 	partition      string
+	installPaths   android.InstallPaths
 }
 
 func sdkDeps(ctx android.BottomUpMutatorContext, sdkContext android.SdkContext, d dexer) {
