5a5dc6f8662cd7ba8d40751e0b67476ce997400f
Kiyoung Kim <kiyoungkim@google.com>
2024-07-08 11:02:23 +0900

    Add 'use_debug_art' to product_variable
    Add 'use_debug_art' property to product_variable so it can be checked
    from Android.bp whether ART debug APEX or ART APEX should be used.
    
    Bug: 351698657
    Test: AOSP CF eng build succeeded
    Test: AOSP CF userdebug build succeeded
    Change-Id: I01cd80f0d899c55828ecb05fdafdc9bb05fd8200

--- android/config.go
+++ android/config.go
@@ -2093,3 +2093,11 @@ func (c *config) BoardUseVbmetaDigestInFingerprint() bool {
 func (c *config) OemProperties() []string {
 	return c.productVariables.OemProperties
 }
+
+func (c *config) UseDebugArt() bool {
+	if c.productVariables.ArtTargetIncludeDebugBuild != nil {
+		return Bool(c.productVariables.ArtTargetIncludeDebugBuild)
+	}
+
+	return Bool(c.productVariables.Eng)
+}
