e410b0f940551a7147bc9536940a9e55104e60c5
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-04-12 14:16:01 +0300

    Make install_symlink symlink_target configurable
    Test: m
    Change-Id: I08105946e8406c90096ad19c78e1ba63a2df2341

--- etc/install_symlink.go
+++ etc/install_symlink.go
@@ -18,6 +18,8 @@ import (
 	"android/soong/android"
 	"path/filepath"
 	"strings"
+
+	"github.com/google/blueprint/proptools"
 )
 
 func init() {
@@ -52,7 +54,7 @@ type InstallSymlinkProperties struct {
 	// properties.
 	Installed_location string
 	// The target of the symlink, aka where the symlink points.
-	Symlink_target string
+	Symlink_target proptools.Configurable[string]
 }
 
 type InstallSymlink struct {
@@ -64,7 +66,8 @@ type InstallSymlink struct {
 }
 
 func (m *InstallSymlink) GenerateAndroidBuildActions(ctx android.ModuleContext) {
-	if filepath.Clean(m.properties.Symlink_target) != m.properties.Symlink_target {
+	symlink_target := m.properties.Symlink_target.GetOrDefault(ctx, "")
+	if filepath.Clean(symlink_target) != symlink_target {
 		ctx.PropertyErrorf("symlink_target", "Should be a clean filepath")
 		return
 	}
@@ -83,7 +86,7 @@ func (m *InstallSymlink) GenerateAndroidBuildActions(ctx android.ModuleContext) 
 
 	name := filepath.Base(m.properties.Installed_location)
 	installDir := android.PathForModuleInstall(ctx, filepath.Dir(m.properties.Installed_location))
-	m.installedPath = ctx.InstallAbsoluteSymlink(installDir, name, m.properties.Symlink_target)
+	m.installedPath = ctx.InstallAbsoluteSymlink(installDir, name, symlink_target)
 }
 
 func (m *InstallSymlink) AndroidMkEntries() []android.AndroidMkEntries {
