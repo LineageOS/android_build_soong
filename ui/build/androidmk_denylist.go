ae3ed2692f6b37f20e8c7e15a48fdf0c20d18cb2
Yumi Yukimura <me.cafebabe@gmail.com>
2025-06-12 13:53:51 +0000

    androidmk_denylist: Allow external/mesa/android/Android.mk
    Change-Id: I61fdb0ea33f86a05413df6faf9501d6e4958cd43

--- ui/build/androidmk_denylist.go
+++ ui/build/androidmk_denylist.go
@@ -62,6 +62,7 @@ var androidmk_denylist []string = []string{
 var androidmk_allowlist []string = []string{
 	"art/Android.mk",
 	"bootable/deprecated-ota/updater/Android.mk",
+	"external/mesa/android/Android.mk",
 	"tools/vendor/google_prebuilts/arc/Android.mk",
 }
 
