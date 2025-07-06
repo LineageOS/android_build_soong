7033aa5174c8c0b006d9e75e8926ebc557b5482a
Arian <arian.kulmer@web.de>
2025-07-06 19:07:37 +0200

    Let qcom audio/display headers resolve to generated_kernel_headers too
    Change-Id: I9ff37861046c07c466dff9b15d852800f15a9c37

--- cc/cc.go
+++ cc/cc.go
@@ -2943,7 +2943,13 @@ func (c *Module) DepsMutator(actx android.BottomUpMutatorContext) {
 			for _, entry := range list {
 				// Replace device_kernel_headers with generated_kernel_headers
 				// for inline kernel building
-				if entry == "device_kernel_headers" || entry == "qti_kernel_headers" || entry == "generated_kernel_headers" {
+				switch entry {
+				case "audio_kernel_headers",
+						"device_kernel_headers",
+						"generated_kernel_headers",
+						"qti_audio_kernel_uapi",
+						"qti_display_kernel_headers",
+						"qti_kernel_headers":
 					if (len(ctx.Config().Getenv("TARGET_PREBUILT_KERNEL_HEADERS")) > 0) {
 						newHeaderLibs = append(newHeaderLibs, "prebuilt_kernel_headers")
 						continue
