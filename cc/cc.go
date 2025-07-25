617b0c4daadadcc1ceb055017bab375e7472ba38
Aaron Kling <webgeek1234@gmail.com>
2025-07-24 20:15:45 -0500

    Support inline kernel platform builds
    Change-Id: Ic6d898044331e5448cc8ac429c35dee7e96697bb

--- cc/cc.go
+++ cc/cc.go
@@ -2547,7 +2547,7 @@ func (c *Module) DepsMutator(actx android.BottomUpMutatorContext) {
 				// Replace device_kernel_headers with generated_kernel_headers
 				// for inline kernel building
 				if entry == "device_kernel_headers" || entry == "qti_kernel_headers" || entry == "generated_kernel_headers" {
-					if (len(ctx.Config().Getenv("TARGET_PREBUILT_KERNEL_HEADERS")) > 0) {
+					if (ctx.Config().Getenv("HAS_PREBUILT_KERNEL_HEADERS") == "true") {
 						newHeaderLibs = append(newHeaderLibs, "prebuilt_kernel_headers")
 						continue
 					} else if (ctx.Config().Getenv("INLINE_KERNEL_BUILDING") == "true" && entry != "generated_kernel_headers") {
