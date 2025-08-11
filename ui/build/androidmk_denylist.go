1aeea3a78929c5d12cf15d6ad044148fe7872ca0
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-12 01:45:04 +0800

    androidmk_denylist: Load Android.mk allowlist from vendor/extra
    Change-Id: I3fd203dd4252ac5a4c66dad9700d2a0a3536fe01

--- ui/build/androidmk_denylist.go
+++ ui/build/androidmk_denylist.go
@@ -83,6 +83,7 @@ func blockAndroidMks(ctx Context, androidMks []string) {
 		"vendor/google/build/androidmk/allowlist.txt",
 		"device/google/clockwork/build/androidmk/allowlist.txt",
 		"device/google/sdv/androidmk/allowlist.txt",
+		"vendor/extra/build/androidmk/allowlist.txt",
 	}
 	for _, allowlist_file := range allowlist_files {
 		allowlist := getAllLines(ctx, allowlist_file)
