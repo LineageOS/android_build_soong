70b662c171c0e006f7c81fefe5be3388cbb0cf20
LuK1337 <priv.luk@gmail.com>
2025-11-18 12:16:25 +0100

    gen_build_prop: Use short build number for userdebug
    Change-Id: I434dddd4f1500f56660840d82e901604e2873d71

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -219,7 +219,7 @@ def generate_build_info(args):
   print(f"ro.build.uuid?={config['BuildUUID']}")
 
   # ro.build.display.id is shown under Settings -> About Phone
-  if config["BuildVariant"] == "user":
+  if config["BuildVariant"] != "eng":
     # User builds should show:
     # release build number or branch.buld_number non-release builds
 
