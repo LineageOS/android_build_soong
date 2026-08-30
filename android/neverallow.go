123c50928cd30236c70eef38492a628e05d98e64
Bruno Martins <bgcngm@gmail.com>
2026-08-30 17:01:11 +0100

    Allow prebuilt_first_stage_ramdisk to be used
    Change-Id: I30ac6997348c0d51c2ed97e6e218462e8e773ea7

--- android/neverallow.go
+++ android/neverallow.go
@@ -406,7 +406,6 @@ func createPrebuiltEtcBpDefineRule() Rule {
 			"prebuilt_vendor",
 			"prebuilt_sbin",
 			"prebuilt_system",
-			"prebuilt_first_stage_ramdisk",
 			"prebuilt_radio",
 			"prebuilt_gpu",
 			"prebuilt_vendor_overlay",
