144749cb14fe5cb6d74a3958d2a6a217a14bb4e1
LuK1337 <priv.luk@gmail.com>
2026-09-23 19:03:05 +0200

    sandbox_linux: Make .repo/manifests.git writable
    `repo manifest -r` wants to update .repo_config.json inside of it.
    
    Change-Id: Ib53b49af5f727f72b62737eab2ba0205309c9bca

--- ui/build/sandbox_linux.go
+++ ui/build/sandbox_linux.go
@@ -308,6 +308,9 @@ func (c *Cmd) wrapSandbox() {
 		//Mount out dir as read-write
 		"-B", c.outDirArg(),
 
+		// Make .repo/manifests.git writable
+		"-B", filepath.Join(c.srcDirArg(), ".repo/manifests.git"),
+
 		// Disable newcgroup for now, since it may require newer kernels
 		// TODO: try out cgroups
 		"--disable_clone_newcgroup",
